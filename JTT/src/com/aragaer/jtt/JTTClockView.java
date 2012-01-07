fba04ca26203b90d1a2b57084e173051e853fdb2
Aragaer <aragaer@gmail.com>
2012-01-07 15:32:47 +0400

    Better clock drawing code

--- JTT/src/com/aragaer/jtt/JTTClockView.java
+++ JTT/src/com/aragaer/jtt/JTTClockView.java
@@ -13,165 +13,160 @@ import android.util.AttributeSet;
 import android.widget.TextView;
 
 public class JTTClockView extends TextView {
-	private final static int step = 360 / 12;
-	private final static float gap = 1.5f;
-	
-	private final Paint mStrokePaint = new Paint();
-	private final Paint mSolidPaint = new Paint();
-	private final Paint mSolidPaint2 = new Paint();
-	private final Bitmap clocks_h[] = new Bitmap[12], clocks_v[] = new Bitmap[12]; 
-	private JTTHour hour;
-	public JTTClockView(Context context) {
-		super(context);
-	}
+    private final Paint mStrokePaint = new Paint();
+    private final Paint mStrokePaint2 = new Paint();
+    private final Paint mSolidPaint = new Paint();
+    private final Paint mSolidPaint2 = new Paint();
+    private final Bitmap ch[] = new Bitmap[12], cv[] = new Bitmap[12];
+    private JTTHour hour;
 
-	public JTTClockView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
-	
-	public JTTClockView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-	}
-	
-	@Override
-	protected void onDraw(Canvas canvas) {
-		if (hour == null)
-			return;
-		
-		int w = super.getWidth();
-		int h = super.getHeight();
-		
-		Bitmap[] clocks;
-		int size;
-		
-		if (h > w) {
-			clocks = clocks_v;
-			size = w;
-		} else {
-			clocks = clocks_h;
-			size = h;
-		}
-		
-		if (clocks[hour.num] == null)
-			clocks[hour.num] = drawBitmap(hour.num, size);
-		
-		Paint paint = new Paint();
-		paint.setAntiAlias(true);
-		paint.setFilterBitmap(true);
-		paint.setDither(true);
-		
-		Matrix transform = new Matrix();
-		transform.setTranslate(w/2 - size/2, 3*h/5 - size/2);
-		transform.preRotate(step*(0.5f - hour.num - hour.fraction), size/2, size/2);
-		canvas.drawBitmap(clocks[hour.num], transform, paint);
-		
-		paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setTextAlign(Paint.Align.CENTER);
-        paint.setColor(Color.argb(255, 255, 255, 255));
-        paint.setTextSize(h/20);
-        
-        canvas.drawText("Hour of "+hour.hour, w/2, h/10, paint);
-	}
-	
+    private Context ctx;
+
+    public JTTClockView(Context context) {
+        this(context, null, 0);
+    }
+
+    public JTTClockView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public JTTClockView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        ctx = context;
+        setupPaint();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (hour == null)
+            return;
+
+        final int w = getWidth();
+        final int h = getHeight();
+        final Boolean v = h > w;
+        final Bitmap[] clocks = v ? cv : ch;
+        final int size = v ? w / 2 : h / 2;
+
+        if (clocks[hour.num] == null)
+            clocks[hour.num] = drawBitmap(hour.num, size);
+
+        final Matrix m = new Matrix();
+        m.setTranslate(v ? 0 : 3 * w / 5 - size, v ? 3 * h / 5 - size : 0);
+        m.preRotate(step * (0.5f - hour.num - hour.fraction), size, size);
+        canvas.drawBitmap(clocks[hour.num], m, mStrokePaint2);
+
+        mStrokePaint2.setTextSize(v ? w / 20 : w / 15);
+
+        final String s = (v ? ctx.getString(R.string.hr_of) + " " : "");
+        canvas.drawText(s + hour.hour, v ? size : w / 5, v ? h / 10 : size,
+                mStrokePaint2);
+    }
+
     private final void setupPaint() {
-    	LightingColorFilter f = new LightingColorFilter(0xFFFFFFFF, 0xFFCCCCCC);
+        LightingColorFilter f = new LightingColorFilter(0xFFFFFFFF, 0xFFCCCCCC);
         mStrokePaint.setAntiAlias(true);
         mStrokePaint.setStyle(Paint.Style.STROKE);
         mStrokePaint.setTextAlign(Paint.Align.CENTER);
-        mStrokePaint.setColor(Color.argb(192, 0, 0, 0));
+        mStrokePaint.setColor(Color.parseColor(ctx.getString(R.color.stroke)));
+
+        mStrokePaint2.setAntiAlias(true);
+        mStrokePaint2.setFilterBitmap(true);
+        mStrokePaint2.setDither(true);
+        mStrokePaint2.setTextAlign(Paint.Align.CENTER);
+        mStrokePaint2.setStyle(Paint.Style.STROKE);
+        mStrokePaint2.setColor(Color.parseColor(ctx
+                .getString(R.color.tab_active)));
 
         mSolidPaint.setAntiAlias(true);
         mSolidPaint.setStyle(Paint.Style.FILL);
         mSolidPaint.setTextAlign(Paint.Align.CENTER);
-        mSolidPaint.setColor(R.color.fill);
+        mSolidPaint.setColor(Color.parseColor(ctx.getString(R.color.fill)));
         mSolidPaint.setColorFilter(f);
 
         mSolidPaint2.setAntiAlias(true);
         mSolidPaint2.setStyle(Paint.Style.FILL);
         mSolidPaint2.setTextAlign(Paint.Align.CENTER);
-        mSolidPaint2.setColor(Color.argb(128, 128, 128, 128));
+        mSolidPaint2.setColor(Color.parseColor(ctx.getString(R.color.night)));
         mSolidPaint2.setStrokeWidth(1.3f);
     }
-    
-    private Bitmap drawBitmap(int num, int size) {
-		setupPaint();
 
-		int cx = size/2;
-    	int cy = size/2;
-		Bitmap result = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
-		Canvas canvas = new Canvas(result);
-		
-		int innerR = size/5;
-		int thick = size/5;
-		int outerR = innerR+thick;
-		int selR = outerR + thick/4;
-		
-		mStrokePaint.setTextSize(thick/3);
-		mSolidPaint2.setTextSize(thick/3);
-		
-		RectF outer = new RectF(cx-outerR,cy-outerR,cx+outerR,cy+outerR);
-		RectF inner = new RectF(cx-innerR,cy-innerR,cx+innerR,cy+innerR);
-		RectF sel = new RectF(cx-selR,cy-selR,cx+selR,cy+selR);
-		RectF sun = new RectF(cx-innerR*0.2f, cy-innerR*0.2f, cx+innerR*0.2f, cy+innerR*0.2f);
-		
-		canvas.rotate(-step/2,cx,cy);
-		canvas.translate(-innerR*0.75f, 0);
-		canvas.drawArc(sun, 0, 360, false, mSolidPaint);
-		canvas.drawArc(sun, 0, 360, false, mStrokePaint);
-		canvas.rotate(90, cx+innerR*0.75f, cy);
-		canvas.drawArc(sun, 0, 180, false, mSolidPaint);
-		canvas.drawArc(sun, 180, 180, false, mSolidPaint2);
-		canvas.drawArc(sun, 0, 360, false, mStrokePaint);
-		canvas.drawLine(cx-innerR*0.2f, cy, cx+innerR*0.2f, cy, mStrokePaint);
-		canvas.rotate(90, cx+innerR*0.75f, cy);
-		canvas.drawArc(sun, 0, 360, false, mSolidPaint2);
-		canvas.drawArc(sun, 0, 360, false, mStrokePaint);
-		canvas.rotate(90, cx+innerR*0.75f, cy);
-		canvas.drawArc(sun, 180, 180, false, mSolidPaint);
-		canvas.drawArc(sun, 0, 180, false, mSolidPaint2);
-		canvas.drawArc(sun, 0, 360, false, mStrokePaint);
-		canvas.drawLine(cx-innerR*0.2f, cy, cx+innerR*0.2f, cy, mStrokePaint);
-		canvas.translate(innerR*0.75f, 0);
-		canvas.rotate(90 + step/2,cx,cy);
-		
-		int arc_start = -90-Math.round(step/2-gap);
-		int arc_end = -90+Math.round(step/2-gap);
-		int arc_len = arc_end - arc_start;
+    private final static int step = 360 / 12;
+    private final static float gap = 1.5f;
 
-		double start = Math.toRadians(arc_start);
-		double end = Math.toRadians(arc_end);
-		
-		for (int hr = 0; hr < 12; hr++) {
-			Path path = new Path();
-			path.addArc(inner, arc_start, arc_len);
-			if (hr == num) {
-				path.lineTo((float)(cx + selR*Math.cos(end)), (float)(cy+selR*Math.sin(end)));
-				path.addArc(sel, arc_end, -arc_len);
-			} else {
-				path.lineTo((float)(cx + outerR*Math.cos(end)), (float)(cy+outerR*Math.sin(end)));
-				path.addArc(outer, arc_end, -arc_len);
-			}
-			path.lineTo((float)(cx + innerR*Math.cos(start)), (float)(cy+innerR*Math.sin(start)));
-			canvas.drawPath(path, mSolidPaint);
-			canvas.drawPath(path, mStrokePaint);
-			
-			float glyph_y;
-			if (hr == num)
-				glyph_y = cy - innerR - 5*thick/9;
-			else
-				glyph_y = cy - innerR - 4*thick/9;
-			canvas.drawText(JTT.Glyphs[hr], cx, glyph_y, mSolidPaint2);
-			canvas.drawText(JTT.Glyphs[hr], cx, glyph_y, mStrokePaint);
-			canvas.rotate(step, cx, cy);
-		}
-		
-		return result;
-	}
-    
+    private Bitmap drawBitmap(int num, int c) {
+        Bitmap result = Bitmap.createBitmap(c * 2, c * 2,
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(result);
+
+        final int iR = 2 * c / 5;
+        final int thick = 2 * c / 5;
+        final int oR = iR + thick;
+        final int selR = oR + thick / 4;
+        final float sR = iR * 0.2f;
+
+        mStrokePaint.setTextSize(thick / 3);
+        mSolidPaint2.setTextSize(thick / 3);
+
+        final RectF outer = new RectF(c - oR, c - oR, c + oR, c + oR);
+        final RectF inner = new RectF(c - iR, c - iR, c + iR, c + iR);
+        final RectF sel = new RectF(c - selR, c - selR, c + selR, c + selR);
+        final RectF sun = new RectF(c - sR, c - sR, c + sR, c + sR);
+
+        canvas.rotate(-step / 2, c, c);
+        canvas.translate(-iR * 0.75f, 0);
+        canvas.drawArc(sun, 0, 360, false, mSolidPaint);
+        canvas.drawArc(sun, 0, 360, false, mStrokePaint);
+        canvas.rotate(90, c + iR * 0.75f, c);
+        canvas.drawArc(sun, 0, 180, false, mSolidPaint);
+        canvas.drawArc(sun, 180, 180, false, mSolidPaint2);
+        canvas.drawArc(sun, 0, 360, false, mStrokePaint);
+        canvas.drawLine(c - sR, c, c + sR, c, mStrokePaint);
+        canvas.rotate(90, c + iR * 0.75f, c);
+        canvas.drawArc(sun, 0, 360, false, mSolidPaint2);
+        canvas.drawArc(sun, 0, 360, false, mStrokePaint);
+        canvas.rotate(90, c + iR * 0.75f, c);
+        canvas.drawArc(sun, 180, 180, false, mSolidPaint);
+        canvas.drawArc(sun, 0, 180, false, mSolidPaint2);
+        canvas.drawArc(sun, 0, 360, false, mStrokePaint);
+        canvas.drawLine(c - sR, c, c + sR, c, mStrokePaint);
+        canvas.translate(iR * 0.75f, 0);
+        canvas.rotate(90 + step / 2, c, c);
+
+        final int arc_start = -90 - Math.round(step / 2 - gap);
+        final int arc_end = -90 + Math.round(step / 2 - gap);
+        final int arc_len = arc_end - arc_start;
+
+        final double start = Math.toRadians(arc_start);
+        final double end = Math.toRadians(arc_end);
+        final float l2x = c + (float) Math.cos(start) * iR;
+        final float l2y = c + (float) Math.sin(start) * iR;
+        final float l1x = c + (float) Math.cos(end) * oR;
+        final float l1y = c + (float) Math.sin(end) * oR;
+        final float l1xs = c + (float) Math.cos(end) * selR;
+        final float l1ys = c + (float) Math.sin(end) * selR;
+
+        for (int hr = 0; hr < 12; hr++) {
+            final Path path = new Path();
+            final Boolean current = hr == num;
+
+            path.addArc(inner, arc_start, arc_len);
+            path.lineTo(current ? l1xs : l1x, current ? l1ys : l1y);
+            path.addArc(current ? sel : outer, arc_end, -arc_len);
+            path.lineTo(l2x, l2y);
+            canvas.drawPath(path, mSolidPaint);
+            canvas.drawPath(path, mStrokePaint);
+
+            final float glyph_y = c - iR - (current ? 5 : 4) * thick / 9;
+            canvas.drawText(JTT.Glyphs[hr], c, glyph_y, mSolidPaint2);
+            canvas.drawText(JTT.Glyphs[hr], c, glyph_y, mStrokePaint);
+            canvas.rotate(step, c, c);
+        }
+
+        return result;
+    }
+
     public void setJTTHour(JTTHour new_hour) {
-    	hour = new_hour;
-    	invalidate();
+        hour = new_hour;
+        invalidate();
     }
 }
