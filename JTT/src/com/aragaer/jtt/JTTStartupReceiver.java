0f92a1bf20e4573601d78b40b40d3cf4e853c091
Aragaer <aragaer@gmail.com>
2012-01-10 09:24:45 +0400

    Startup preference should work properly now

--- JTT/src/com/aragaer/jtt/JTTStartupReceiver.java
+++ JTT/src/com/aragaer/jtt/JTTStartupReceiver.java
@@ -3,9 +3,13 @@ package com.aragaer.jtt;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.preference.PreferenceManager;
 
 public class JTTStartupReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
-        context.startService(new Intent(context, JTTService.class));
+        if (PreferenceManager
+                .getDefaultSharedPreferences(context)
+                .getBoolean("jtt_bootup", true))
+            context.startService(new Intent(context, JTTService.class));
     }
 }
