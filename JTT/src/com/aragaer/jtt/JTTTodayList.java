6189fcae1eb23e346eb894d10daa024f8048eb8f
Aragaer <aragaer@gmail.com>
2012-03-12 01:10:33 +0400

    WIP

---
+++ JTT/src/com/aragaer/jtt/JTTTodayList.java
@@ -0,0 +1,10 @@
+package com.aragaer.jtt;
+
+import android.content.Context;
+import android.widget.ListView;
+
+public class JTTTodayList extends ListView {
+    public JTTTodayList(Context context) {
+        super(context);
+    }
+}
