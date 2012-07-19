9aed9e818c22929003a5b4e66a61a805c07270db
Ilya Konovalov <aragaer@gmail.com>
2012-07-19 11:59:29 +0400

    Another rewrite of TodayList. WIP.

--- JTT/src/com/aragaer/jtt/JTTTodayList.java
+++ JTT/src/com/aragaer/jtt/JTTTodayList.java
@@ -2,9 +2,9 @@ package com.aragaer.jtt;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Collections;
 import java.util.Date;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.TimeZone;
 
 import android.content.Context;
@@ -21,7 +21,6 @@ import android.widget.TextView;
 public class JTTTodayList extends ListView {
     private static final String TAG = JTTTodayList.class.getSimpleName();
 
-    private LinkedList<TodayItem> inner = new LinkedList<TodayItem>();
     private LinkedList<Long> transitions = new LinkedList<Long>();
 
     /* sets a value to a text field */
@@ -29,44 +28,49 @@ public class JTTTodayList extends ListView {
         ((TextView) v.findViewById(id)).setText(t);
     }
 
-    /* A single item in TodayList */
-    private static class TodayItem {
+    private static abstract class TodayItem {
         public final long time;
+        abstract public View toView(Context c);
+        public TodayItem(long t) {
+            time = t;
+        }
+    }
+
+    /* A single item in TodayList */
+    private static class HourItem extends TodayItem {
+        public static int current;
+        public static long next_transition;
         public final int hnum;
         public final String date;
 
-        public TodayItem(long t, int h) {
-            time = t;
+        public HourItem(long t, int h) {
+            super(t);
             hnum = h % 12;
-            date = h == -1 ? null : df.format(new Date(t));
+            date = df.format(new Date(t));
         }
 
-        static Context ctx = null;
-        static String[] extras = null;
-        static JTTHour.StringsHelper sh = null;
+        static String[] extras = null, hours = null;
 
-        protected void updateCtx(Context c) {
-            /* as long as only one context is used, keep these objects */
-            if (!c.equals(ctx)) {
-                sh = new JTTHour.StringsHelper(c);
+        public View toView(Context c) {
+            if (extras == null) {
                 extras = new String[] { c.getString(R.string.sunset), "", "",
                         c.getString(R.string.midnight), "", "",
                         c.getString(R.string.sunrise), "", "",
                         c.getString(R.string.midday), "", "" };
-                ctx = c;
-            }
-        }
+                hours = c.getResources().getStringArray(R.array.hour_of);
 
-        public View toView(Context c) {
-            updateCtx(c);
+            }
             LayoutInflater li = (LayoutInflater) c
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View v = li.inflate(R.layout.today_item, null);
-            boolean is_current = false; // TODO: fix this!
+
+            /* no need to check for previous transition */
+            boolean is_current = hnum == current
+                        && System.currentTimeMillis() < next_transition;
 
             t(v, R.id.time, date);
             t(v, R.id.glyph, JTTHour.Glyphs[hnum]);
-            t(v, R.id.name, sh.getHrOf(hnum));
+            t(v, R.id.name, hours[hnum]);
             t(v, R.id.extra, extras[hnum]);
             t(v, R.id.curr, is_current ? "▶" : "");
 
@@ -76,23 +80,11 @@ public class JTTTodayList extends ListView {
 
     /* "DayName" item in Today List */
     private static class DayItem extends TodayItem {
-        static String[] daynames = null;
         public DayItem(long t) {
-            super(t, -1);
+            super(t);
         }
 
-        @Override
-        protected void updateCtx(Context c) {
-            if (!c.equals(ctx))
-                daynames = new String[] { c.getString(R.string.day_next),
-                        c.getString(R.string.day_curr),
-                        c.getString(R.string.day_prev) };
-            super.updateCtx(c);
-        }
-
-        @Override
         public View toView(Context c) {
-            updateCtx(c);
             LayoutInflater li = (LayoutInflater) c
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View v = li.inflate(android.R.layout.preference_category, null);
@@ -102,9 +94,15 @@ public class JTTTodayList extends ListView {
         }
 
         /* returns strings like "today" or "2 days ago" etc */
-        String dateToString(long date, Context c) {
-            final long today = ms_to_day(System.currentTimeMillis());
-            final int ddiff = (int) (today - ms_to_day(date));
+        static String[] daynames = null;
+        private String dateToString(long date, Context c) {
+            if (daynames == null)
+                daynames = new String[] { c.getString(R.string.day_next),
+                    c.getString(R.string.day_curr),
+                    c.getString(R.string.day_prev) };
+
+            final long now = System.currentTimeMillis();
+            final int ddiff = (int) (ms_to_day(now) - ms_to_day(date));
             if (ddiff < 2 && ddiff > -2)
                 return daynames[ddiff+1];
             final Resources r = c.getResources();
@@ -116,9 +114,8 @@ public class JTTTodayList extends ListView {
 
     private final TodayAdapter ta;
     private final JTTMainActivity main;
-    long jdn_f, jdn_l;
+    long jdn_min, jdn_max;
     private static final DateFormat df = new SimpleDateFormat("HH:mm");
-    private long forward_sync = 0, backward_sync = 0;
 
     public JTTTodayList(Context context) {
         super(context);
@@ -134,54 +131,34 @@ public class JTTTodayList extends ListView {
      * these two go to the beginning or to the end of the list
      */
     public void addTransitions(Bundle b) {
-        boolean forward;
         long jdn = b.getLong("jdn");
         long sunrise = b.getLong("sunrise");
         long sunset = b.getLong("sunset");
 
-        if (inner.isEmpty()) {
-            forward = true;
-            inner.add(new TodayItem(sunrise, 6));
-            jdn_l = jdn_f = jdn;
-        } else { // list is not empty, add one night to list
-            if (jdn <= jdn_l && jdn >= jdn_f) {
-                Log.wtf(TAG, "Got "+jdn+" which is between "+jdn_f+" and "+jdn_l);
+        if (transitions.isEmpty()) {
+            transitions.add(sunrise);
+            transitions.add(sunset);
+            jdn_max = jdn_min = jdn;
+        } else {
+            if (jdn_max < jdn) {
+                transitions.add(sunrise);
+                transitions.add(sunset);
+                jdn_max = jdn;
+            } else if (jdn_min > jdn) { // add to front
+                transitions.addFirst(sunset);
+                transitions.addFirst(sunrise);
+                jdn_min = jdn;
+            } else {
+                Log.wtf(TAG, "Got "+jdn+" which is between "+jdn_min+" and "+jdn_max);
                 return;
             }
-            forward = jdn_l < jdn;
-            add_interval(forward ? sunrise : sunset, !forward, false);
         }
 
-        // now add one day to list
-        if (forward) {
-            jdn_l = jdn;
-            add_interval(sunset, false, true);
-        } else {
-            jdn_f = jdn;
-            add_interval(sunrise, true, true);
-        }
         updateItems();
     }
 
-    /* split the interval into 6 equal parts and add them to the list */
-    private void add_interval(long tr, boolean to_front, boolean day) {
-        if (to_front) {
-            long start = inner.getFirst().time;
-            long diff = start - tr;
-            int add = (day ? 12 : 6);
-            for (int i = -1; i >= -6; i--)
-                inner.addFirst(new TodayItem(start + i * diff / 6, add + i));
-        } else {
-            long start = inner.getLast().time;
-            long diff = tr - start;
-            int add = (day ? 6 : 0);
-            for (int i = 1; i <= 6; i++)
-                inner.addLast(new TodayItem(start + i * diff / 6, add + i));
-        }
-    }
-
     public void reset() {
-        inner.clear();
+        transitions.clear();
     }
 
     protected void onServiceConnect() {
@@ -197,51 +174,73 @@ public class JTTTodayList extends ListView {
     }
 
     private void getPastDay() {
-        getDay(jdn_f - 1);
+        getDay(jdn_min - 1);
     }
 
     private void getFutureDay() {
-        getDay(jdn_l + 1);
+        getDay(jdn_max + 1);
     }
 
-    // TODO: remove obsolete items
-    private static final int PAD = 6;
     private void updateItems() {
-        // first find out to which interval the current time belongs
-        long now = System.currentTimeMillis();
-        final int is = inner.size();
-        if (inner.get(PAD).time > now) {
+        if (transitions.isEmpty() || (transitions.size() % 2) == 1) {
+            Log.wtf(TAG, "Transitions list is empty or has incorrect number of items");
+            reset();
+            return;
+        }
+
+        int pos = Collections.binarySearch(transitions, System.currentTimeMillis());
+        /* current time falls between pos and pos+1 */
+        if (pos < 0)
+            pos = -pos - 2;
+
+        /* check if we have enough data for past */
+        if (pos < 1) {
             getPastDay();
             return;
         }
-        if (inner.get(is - PAD - 1).time < now) {
+
+        /* remove outdated stuff */
+        while (pos > 2) {
+            transitions.remove();
+            transitions.remove();
+            pos -= 2;
+            jdn_min++;
+        }
+
+        /* now pos is exactly 1 or 2
+         * 1 means it is night now
+         * 2 means it is day now
+         */
+
+        /* check if we have enough data for future */
+        if (transitions.size() <= pos + 2) {
             getFutureDay();
             return;
         }
-        int i = PAD + 1;
-        while (inner.get(i).time < now)
-            i++;
-        int low = (i - PAD - 1) / 6 * 6;
-        int high = (i + PAD + 5) / 6 * 6;
-        ta.buildItems(inner.subList(low, high + 1));
+
+        /* it is possible that we have too much "future" information
+         * keep it
+         */
+
+        HourItem.next_transition = transitions.get(pos + 1);
+
+        /* exactly 4 transitions are used:
+         * pos-1, pos, pos+1 and pos+2
+         */
+        long[] l = new long[4];
+        for (int i = 0; i < 4; i++)
+            l[i] = transitions.get(pos - 1 + i);
+
+        /* if it is day now then first interval is night */
+        ta.buildItems(l, pos == 1);
     }
 
     public void setCurrent(int cur) {
-        long now = System.currentTimeMillis();
-        ta.cur = cur;
-        /* if everything is in valid state, that's simple */
-        if (now < forward_sync && now >= backward_sync) {
-            ta.notifyDataSetChanged();
-            return;
-        }
-
-        // here's some redrawing
+        HourItem.current = cur;
         ta.notifyDataSetChanged();
     }
 
     private static class TodayAdapter extends ArrayAdapter<TodayItem> {
-        protected int cur;
-
         public TodayAdapter(Context c, int layout_id) {
             super(c, layout_id);
         }
@@ -254,23 +253,36 @@ public class JTTTodayList extends ListView {
         /* takes a sublist of hours
          * creates a list to display by adding day names
          */
-        private void buildItems(List<TodayItem> in) {
+        private void buildItems(long[] transitions, boolean is_day) {
             clear();
 
-            /* day is time stamp for 00:00:00 */
-            long day = in.get(0).time;
+            /* time stamp for 00:00:00 */
+            long start_of_day = transitions[0];
 
             /* "aligning" code */
-            day += TimeZone.getDefault().getOffset(day);
-            day -= day % JTT.ms_per_day;
-            day -= TimeZone.getDefault().getOffset(day);
+            start_of_day += TimeZone.getDefault().getOffset(start_of_day);
+            start_of_day -= start_of_day % JTT.ms_per_day;
+            start_of_day -= TimeZone.getDefault().getOffset(start_of_day);
 
-            for (TodayItem i : in) {
-                if (i.time >= day) {
-                    add(new DayItem(day));
-                    day += JTT.ms_per_day;
+            add(new DayItem(start_of_day)); // List should start with one
+            start_of_day += JTT.ms_per_day;
+
+            int h_add = is_day ? 6 : 0;
+
+            /* start with first transition */
+            add(new HourItem(transitions[0], h_add));
+            for (int i = 1; i < transitions.length; i++) {
+                long start = transitions[i - 1];
+                long diff = transitions[i] - start;
+                for (int j = 1; j <= 6; j++) {
+                    long t = start + j * diff / 6;
+                    if (t >= start_of_day) {
+                        add(new DayItem(start_of_day));
+                        start_of_day += JTT.ms_per_day;
+                    }
+                    add(new HourItem(t, h_add + j));
                 }
-                add(i);
+                h_add = 6 - h_add;
             }
         }
 
