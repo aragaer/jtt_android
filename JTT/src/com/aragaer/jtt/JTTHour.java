0ee644f97ef34a011a0c4b2ddc94f91dbe832fb6
Ilya Konovalov <aragaer@gmail.com>
2012-09-05 01:09:34 +0400

    JTTHour objects can now be reused properly

--- JTT/src/com/aragaer/jtt/JTTHour.java
+++ JTT/src/com/aragaer/jtt/JTTHour.java
@@ -9,9 +9,9 @@ public class JTTHour {
     public static final String Glyphs[] = { "酉", "戌", "亥", "子", "丑", "寅", "卯",
             "辰", "巳", "午", "未", "申" };
 
-    public final boolean isNight;
-    public final int num; // 0 to 11, where 0 is hour of Cock and 11 is hour of Monkey
-    public final int strikes;
+    public boolean isNight;
+    public int num; // 0 to 11, where 0 is hour of Cock and 11 is hour of Monkey
+    public int strikes;
     public int fraction; // 0 to 99
 
     private static final int num_to_strikes(int num) {
@@ -23,6 +23,11 @@ public class JTTHour {
     }
 
     public JTTHour(int n, int f) {
+        this.setTo(n, f);
+    }
+
+    // Instead of reallocation, reuse existing object
+    public void setTo(int n, int f) {
         num = n;
         isNight = n < 6;
         strikes = num_to_strikes(n);
