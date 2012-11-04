e0964b5172f8d95d4fad8a1171e33ef32658039b
Ilya Konovalov <aragaer@gmail.com>
2012-11-05 03:53:11 +0400

    Weak ref and indents in alarm window

--- src/com/aragaer/jtt/JTTAlarmActivity.java
+++ src/com/aragaer/jtt/JTTAlarmActivity.java
@@ -1,5 +1,6 @@
 package com.aragaer.jtt;
 
+import java.lang.ref.WeakReference;
 import java.text.DateFormat;
 
 import android.app.Activity;
@@ -14,70 +15,71 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class JTTAlarmActivity extends Activity {
-    private final static String TAG = JTTAlarmActivity.class.getSimpleName();
-    protected JTTUtil.ConnHelper conn = new JTTUtil.ConnHelper(this, new IncomingHandler(this));
-    private TextView time, glyph, fraction;
-    private JTTAlarm alarm = null;
+	private final static String TAG = JTTAlarmActivity.class.getSimpleName();
+	protected JTTUtil.ConnHelper conn = new JTTUtil.ConnHelper(this, new IncomingHandler(this));
+	private TextView time, glyph, fraction;
+	private JTTAlarm alarm = null;
 
-    static class IncomingHandler extends Handler {
-    	DateFormat df;
-    	JTTAlarmActivity activity;
-        public IncomingHandler(JTTAlarmActivity activity) {
-        	this.activity = activity;
-        	df = android.text.format.DateFormat.getTimeFormat(activity);
+	static class IncomingHandler extends Handler {
+		private final WeakReference<JTTAlarmActivity> weak;
+		DateFormat df;
+		public IncomingHandler(JTTAlarmActivity activity) {
+			weak = new WeakReference<JTTAlarmActivity>(activity);
+			df = android.text.format.DateFormat.getTimeFormat(activity);
 		}
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-            case JTTService.MSG_HOUR:
-                activity.glyph.setText(JTTHour.Glyphs[msg.arg1]);
-                activity.fraction.setText(msg.arg2 + "%");
-                activity.time.setText(df.format(System.currentTimeMillis()));
-                break;
-            default:
-                super.handleMessage(msg);
-            }
-        }
-    }
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case JTTService.MSG_HOUR:
+				JTTAlarmActivity activity = weak.get();
+				activity.glyph.setText(JTTHour.Glyphs[msg.arg1]);
+				activity.fraction.setText(msg.arg2 + "%");
+				activity.time.setText(df.format(System.currentTimeMillis()));
+				break;
+			default:
+				super.handleMessage(msg);
+			}
+		}
+	}
 
-    @Override
-    public void onCreate(Bundle saved) {
-        super.onCreate(saved);
-        final Intent service = new Intent(JTTService.class.getName());
-        startService(service);
-        JTTUtil.initLocale(this);
+	@Override
+	public void onCreate(Bundle saved) {
+		super.onCreate(saved);
+		final Intent service = new Intent(JTTService.class.getName());
+		startService(service);
+		JTTUtil.initLocale(this);
 
-        Bundle b = getIntent().getExtras();
-        if (b != null)
-            alarm = JTTAlarm.load(this, b.getLong("alarm_id"));
-        if (alarm == null)
-            alarm = new JTTAlarm(new JTTHour(0),
-                    this.getString(R.string.unknown_alarm));
+		Bundle b = getIntent().getExtras();
+		if (b != null)
+			alarm = JTTAlarm.load(this, b.getLong("alarm_id"));
+		if (alarm == null)
+			alarm = new JTTAlarm(new JTTHour(0),
+					this.getString(R.string.unknown_alarm));
 
-        setContentView(alarm.time == null ? R.layout.alarm_jtt
-                : R.layout.alarm_time);
+		setContentView(alarm.time == null ? R.layout.alarm_jtt
+				: R.layout.alarm_time);
 
-        OnClickListener close = new OnClickListener() {
-            public void onClick(View v) {
-                JTTAlarmActivity.this.finish();
-            }
-        };
+		OnClickListener close = new OnClickListener() {
+			public void onClick(View v) {
+				JTTAlarmActivity.this.finish();
+			}
+		};
 
-        ((Button) findViewById(R.id.stop)).setOnClickListener(close);
-        ((Button) findViewById(R.id.snooze)).setOnClickListener(close);
+		((Button) findViewById(R.id.stop)).setOnClickListener(close);
+		((Button) findViewById(R.id.snooze)).setOnClickListener(close);
 
-        ((TextView) findViewById(R.id.alarm)).setText(alarm.name);
-        time = (TextView) findViewById(R.id.time);
-        glyph = (TextView) findViewById(R.id.glyph);
-        fraction = (TextView) findViewById(R.id.fraction);
+		((TextView) findViewById(R.id.alarm)).setText(alarm.name);
+		time = (TextView) findViewById(R.id.time);
+		glyph = (TextView) findViewById(R.id.glyph);
+		fraction = (TextView) findViewById(R.id.fraction);
 
-        conn.bind(service, 0);
-    }
+		conn.bind(service, 0);
+	}
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        conn.release();
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		conn.release();
 
-        Log.i(TAG, "Activity destroyed");
-    }
+		Log.i(TAG, "Activity destroyed");
+	}
 }
