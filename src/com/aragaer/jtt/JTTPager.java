c0c53dbe5643622118f3548e885783d27f26e5a7
Ilya Konovalov <aragaer@gmail.com>
2012-11-05 16:25:11 +0400

    Tabs = RadioButtons

--- src/com/aragaer/jtt/JTTPager.java
+++ src/com/aragaer/jtt/JTTPager.java
@@ -1,7 +1,5 @@
 package com.aragaer.jtt;
 
-import java.util.ArrayList;
-
 import android.content.Context;
 import android.content.res.Configuration;
 import android.util.AttributeSet;
@@ -10,14 +8,18 @@ import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.Scroller;
 
 public class JTTPager extends LinearLayout {
 	private JTTPageView pageview;
-	private LinearLayout tablist;
-	protected final ArrayList<Button> tabs = new ArrayList<Button>();
-	private LayoutParams btnlp, lp;
+	private RadioGroup tablist;
+	private LayoutParams lp;
+	private RadioGroup.LayoutParams btnlp = new RadioGroup.LayoutParams(
+			LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1);
 
 	public JTTPager(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -29,7 +31,12 @@ public class JTTPager extends LinearLayout {
 		pageview.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
 				LayoutParams.FILL_PARENT, 1.0f));
 
-		tablist = new LinearLayout(context, attrs);
+		tablist = new RadioGroup(context, attrs);
+		tablist.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				pageview.snapToScreen(checkedId);
+			}
+		});
 		addView(pageview);
 		addView(tablist);
 		setPadding(5, 5, 5, 5);
@@ -38,59 +45,31 @@ public class JTTPager extends LinearLayout {
 			setOrientation(LinearLayout.VERTICAL);
 			tablist.setOrientation(LinearLayout.HORIZONTAL);
 			tablist.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
-					LayoutParams.WRAP_CONTENT, 0.0f));
-			btnlp = new LayoutParams(LayoutParams.WRAP_CONTENT,
-					LayoutParams.FILL_PARENT, 1.0f);
+					LayoutParams.WRAP_CONTENT, 0));
 		} else {
 			setOrientation(LinearLayout.HORIZONTAL);
 			tablist.setOrientation(LinearLayout.VERTICAL);
 			tablist.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-					LayoutParams.FILL_PARENT, 0.0f));
-			btnlp = new LayoutParams(LayoutParams.FILL_PARENT,
-					LayoutParams.WRAP_CONTENT, 1.0f);
+					LayoutParams.FILL_PARENT, 0));
 		}
-
-		for (Button b : tabs)
-			b.setLayoutParams(btnlp);
 	}
-
-	OnClickListener click = new OnClickListener() {
-		public void onClick(View v) {
-			final int btn_id = v.getId();
-			if (pageview.mCurrentScreen != btn_id)
-				pageview.snapToScreen(btn_id);
-		}
-	};
 
 	public int addTab(View view, int resid) {
-		return addTab(view, getContext().getString(resid));
-	}
-
-	public int addTab(View view, String btn) {
-		final int id = tabs.size();
-		final Button b = new Button(getContext(), null);
-		b.setText(btn);
-		b.setOnClickListener(click);
+		final int id = tablist.getChildCount();
+		final RadioButton b = new RadioButton(getContext(), null);
+		b.setText(getContext().getString(resid));
 		b.setId(id);
-		b.setLayoutParams(btnlp);
-		tabs.add(b);
-		tablist.addView(b);
+		tablist.addView(b, btnlp);
 		pageview.addView(view, lp);
 
-		if (pageview.mCurrentScreen == -1) {
-			pageview.mCurrentScreen = 0;
+		if (id == 0)
 			select_tab(0);
-		}
 
 		return id;
 	}
 
-	protected void deselect_tab(int num) {
-		tabs.get(num).setSelected(false);
-	}
-
 	protected void select_tab(int num) {
-		tabs.get(num).setSelected(true);
+		tablist.check(num);
 	}
 
 	public void scrollToScreen(int num) {
@@ -98,24 +77,25 @@ public class JTTPager extends LinearLayout {
 	}
 
 	public int getScreen() {
-		return pageview.mCurrentScreen;
+		return tablist.getCheckedRadioButtonId();
 	}
 
 	private final class JTTPageView extends ViewGroup {
+		Scroller scroller = new Scroller(getContext());
 		private static final int SNAP_VELOCITY = 1000;
 
-		private VelocityTracker mVelocityTracker;
+		private VelocityTracker vt;
 		private int mMaximumVelocity;
 		private int mTouchSlop;
 
-		protected int mCurrentScreen = -1;
+		int mCurrentScreen = -1;
 
-		private float mLastMotionX;
-		private int mScrollX;
+		private float last_x;
+		private int scroll_x;
 
 		private final static int TOUCH_STATE_REST = 0;
 		private final static int TOUCH_STATE_SCROLLING = 1;
-		private int mTouchState = TOUCH_STATE_REST;
+		private int touch_state = TOUCH_STATE_REST;
 
 		public JTTPageView(Context ctx, AttributeSet attrs) {
 			super(ctx, attrs);
@@ -125,64 +105,61 @@ public class JTTPager extends LinearLayout {
 			final ViewConfiguration cfg = ViewConfiguration.get(getContext());
 			mMaximumVelocity = cfg.getScaledMaximumFlingVelocity();
 			mTouchSlop = cfg.getScaledTouchSlop();
+
+			setHorizontalScrollBarEnabled(false);
 		}
 
 		@Override
 		protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 			super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-			final int width = MeasureSpec.getSize(widthMeasureSpec);
-
 			// The children are given the same width and height as the workspace
 			final int count = getChildCount();
 			for (int i = 0; i < count; i++)
 				getChildAt(i).measure(widthMeasureSpec, heightMeasureSpec);
-
-			if (width > 0) {
-				setHorizontalScrollBarEnabled(false);
-				scrollTo(mCurrentScreen * width, 0);
-			}
 		}
 
 		@Override
 		protected void onLayout(boolean changed, int left, int top, int right,
 				int bottom) {
 			int childLeft = 0;
+			final int width = right - left;
 
 			final int count = getChildCount();
 			for (int i = 0; i < count; i++) {
 				final View child = getChildAt(i);
-				final int childWidth = child.getMeasuredWidth();
-				child.layout(childLeft, 0, childLeft + childWidth,
+				child.layout(childLeft, 0, childLeft + width,
 						child.getMeasuredHeight());
-				childLeft += childWidth;
+				childLeft += width;
 			}
+			if (width > 0)
+				scrollTo(mCurrentScreen * width, 0);
 		}
 
 		@Override
 		public boolean onInterceptTouchEvent(MotionEvent ev) {
 			final int action = ev.getAction();
 			if (action == MotionEvent.ACTION_MOVE
-					&& mTouchState != TOUCH_STATE_REST)
+					&& touch_state != TOUCH_STATE_REST)
 				return true;
 
 			final float x = ev.getX();
 			switch (action) {
 			case MotionEvent.ACTION_MOVE:
-				if (Math.abs(x - mLastMotionX) > mTouchSlop)
-					mTouchState = TOUCH_STATE_SCROLLING;
+				if (Math.abs(x - last_x) > mTouchSlop)
+					touch_state = TOUCH_STATE_SCROLLING;
 				break;
 
 			case MotionEvent.ACTION_DOWN:
 				// Remember location of down touch
-				mLastMotionX = x;
-				mTouchState = TOUCH_STATE_REST;
+				last_x = x;
+				touch_state = TOUCH_STATE_REST;
 				break;
 
 			case MotionEvent.ACTION_CANCEL:
 			case MotionEvent.ACTION_UP:
 				// Release the drag
-				mTouchState = TOUCH_STATE_REST;
+				touch_state = TOUCH_STATE_REST;
 				break;
 			}
 
@@ -190,14 +167,14 @@ public class JTTPager extends LinearLayout {
 			 * The only time we want to intercept motion events is if we are in
 			 * the drag mode.
 			 */
-			return mTouchState != TOUCH_STATE_REST;
+			return touch_state != TOUCH_STATE_REST;
 		}
 
 		@Override
 		public boolean onTouchEvent(MotionEvent ev) {
-			if (mVelocityTracker == null)
-				mVelocityTracker = VelocityTracker.obtain();
-			mVelocityTracker.addMovement(ev);
+			if (vt == null)
+				vt = VelocityTracker.obtain();
+			vt.addMovement(ev);
 
 			final int action = ev.getAction();
 			final float x = ev.getX();
@@ -205,29 +182,28 @@ public class JTTPager extends LinearLayout {
 			switch (action) {
 			case MotionEvent.ACTION_DOWN:
 				// Remember where the motion event started
-				mLastMotionX = x;
+				last_x = x;
 				break;
 			case MotionEvent.ACTION_MOVE:
-				int deltaX = (int) (mLastMotionX - x);
-				mTouchState = TOUCH_STATE_SCROLLING;
+				int deltaX = (int) (last_x - x);
+				touch_state = TOUCH_STATE_SCROLLING;
 				// Scroll to follow the motion event
-				mLastMotionX = x;
+				last_x = x;
 
 				final int maxWidth = (getChildCount() - 1) * getWidth();
 
-				if (mScrollX + deltaX < 0)
-					deltaX = -mScrollX;
-				else if (mScrollX + deltaX > maxWidth)
-					deltaX = maxWidth - mScrollX;
+				if (scroll_x + deltaX < 0)
+					deltaX = -scroll_x;
+				else if (scroll_x + deltaX > maxWidth)
+					deltaX = maxWidth - scroll_x;
 
 				scrollBy(deltaX, 0);
 				break;
 			case MotionEvent.ACTION_UP:
-				if (mTouchState == TOUCH_STATE_SCROLLING) {
+				if (touch_state == TOUCH_STATE_SCROLLING) {
 					final int bump = getWidth() / 2 + 1;
-					mVelocityTracker.computeCurrentVelocity(1000,
-							mMaximumVelocity);
-					int velocityX = (int) mVelocityTracker.getXVelocity();
+					vt.computeCurrentVelocity(1000, mMaximumVelocity);
+					int velocityX = (int) vt.getXVelocity();
 
 					if (velocityX > SNAP_VELOCITY && mCurrentScreen > 0)
 						// Fling hard enough to move left
@@ -239,13 +215,13 @@ public class JTTPager extends LinearLayout {
 
 					snapToScreen(mCurrentScreen);
 
-					mVelocityTracker.recycle();
-					mVelocityTracker = null;
+					vt.recycle();
+					vt = null;
 				}
-				mTouchState = TOUCH_STATE_REST;
+				touch_state = TOUCH_STATE_REST;
 				break;
 			case MotionEvent.ACTION_CANCEL:
-				mTouchState = TOUCH_STATE_REST;
+				touch_state = TOUCH_STATE_REST;
 			}
 
 			return true;
@@ -253,14 +229,14 @@ public class JTTPager extends LinearLayout {
 
 		@Override
 		public void scrollBy(int scrollX, int scrollY) {
-			mScrollX += scrollX;
+			scroll_x += scrollX;
 			super.scrollBy(scrollX, scrollY);
 			selectDestination();
 		}
 
 		@Override
 		public void scrollTo(int scrollX, int scrollY) {
-			mScrollX = scrollX;
+			scroll_x = scrollX;
 			super.scrollTo(scrollX, scrollY);
 			selectDestination();
 		}
@@ -269,7 +245,7 @@ public class JTTPager extends LinearLayout {
 			final int w = getWidth();
 			if (w == 0)
 				return;
-			final int whichScreen = (int) (mScrollX + w / 2) / w;
+			final int whichScreen = (int) (scroll_x + w / 2) / w;
 
 			selectScreen(whichScreen);
 		}
@@ -278,11 +254,10 @@ public class JTTPager extends LinearLayout {
 			final int x = getWidth() * whichScreen;
 			selectScreen(whichScreen);
 			super.scrollTo(x, 0);
-			mScrollX = x;
+			scroll_x = x;
 		}
 
 		public void selectScreen(int whichScreen) {
-			deselect_tab(mCurrentScreen);
 			mCurrentScreen = whichScreen;
 			select_tab(mCurrentScreen);
 		}
