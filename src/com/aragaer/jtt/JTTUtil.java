a127e98aaefdd3bb6672f3098c6be98b99792b0d
Ilya Konovalov <aragaer@gmail.com>
2012-12-18 21:48:53 +0400

    Remove unused connection helper

--- src/com/aragaer/jtt/JTTUtil.java
+++ src/com/aragaer/jtt/JTTUtil.java
@@ -4,20 +4,10 @@ import java.text.DateFormat;
 import java.util.HashMap;
 import java.util.Locale;
 
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.res.Configuration;
 import android.content.res.Resources;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
-import android.os.RemoteException;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -91,71 +81,6 @@ public final class JTTUtil {
 
 		public JTTHour makeHour(String hour, int fraction) {
 			return new JTTHour(name_to_num(hour), fraction);
-		}
-	}
-
-	public static class ConnHelper {
-		private Context context;
-		private final static String TAG = "conn helper";
-
-		private Messenger remote = null;
-		private Messenger local = null;
-
-		public ConnHelper(Context ctx, Handler handler) {
-			context = ctx;
-			local = new Messenger(handler);
-		}
-
-		private ServiceConnection conn = new ServiceConnection() {
-			public void onServiceConnected(ComponentName name, IBinder service) {
-				remote = new Messenger(service);
-				try {
-					Message msg = Message.obtain(null,
-							JTTService.MSG_REGISTER_CLIENT);
-					msg.replyTo = local;
-					remote.send(msg);
-					Log.i(TAG, "Service connection established");
-				} catch (RemoteException e) {
-					// In this case the service has crashed before we could even
-					// do anything with it
-					Log.i(TAG, "Service connection can't be established");
-				}
-			}
-
-			public void onServiceDisconnected(ComponentName name) {
-				remote = null;
-				Log.i(TAG, "Service connection closed");
-			}
-		};
-
-		public void send_msg_to_service(int what, Bundle b) {
-			Message msg = Message.obtain(null, what);
-			if (b != null)
-				msg.setData(b);
-
-			msg.replyTo = local;
-			try {
-				remote.send(msg);
-			} catch (RemoteException e) {
-				Log.i(TAG, "Service connection broken");
-			} catch (NullPointerException e) {
-				Log.i(TAG, "Service not connected");
-			}
-		}
-
-		public boolean bind(Intent service, int flags) {
-			return context.bindService(service, conn, flags);
-		}
-
-		public void release() {
-			send_msg_to_service(JTTService.MSG_UNREGISTER_CLIENT, null);
-
-			try {
-				context.unbindService(conn);
-			} catch (Throwable t) {
-				Log.w(TAG, "Failed to unbind from the service", t);
-			}
-			context = null;
 		}
 	}
 
