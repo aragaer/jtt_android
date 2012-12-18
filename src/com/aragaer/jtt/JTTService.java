67b5d250b1b087650cc9f528030ad52d98a2438b
Ilya Konovalov <aragaer@gmail.com>
2012-12-18 21:46:22 +0400

    Remove unused handler

--- src/com/aragaer/jtt/JTTService.java
+++ src/com/aragaer/jtt/JTTService.java
@@ -1,9 +1,5 @@
 package com.aragaer.jtt;
 
-import java.lang.ref.WeakReference;
-import java.util.ArrayList;
-import java.util.Collections;
-
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -11,13 +7,8 @@ import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.os.Handler;
 import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
 import android.os.RemoteException;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -37,109 +28,7 @@ public class JTTService extends Service {
 	private boolean notify, force_stop = false;
 	private static Notification notification;
 
-	private long sync = 0;
-	protected ArrayList<Long> transitions = new ArrayList<Long>();
-	private long start_day, end_day;
 	private String t_start, t_end;
-
-	public static final int MSG_SETTINGS_CHANGE = 0;
-	public static final int MSG_UPDATE_LOCATION = 1;
-	public static final int MSG_REGISTER_CLIENT = 2;
-	public static final int MSG_UNREGISTER_CLIENT = 3;
-	public static final int MSG_HOUR = 4;
-	public static final int MSG_STOP = 5;
-	public static final int MSG_TRANSITIONS = 6;
-	public static final int MSG_INVALIDATE = 7;
-	public static final int MSG_SUBTICK = 8;
-	public static final int MSG_SYNC = 9;
-
-	private int hour, quarter, part;
-
-	static class JTTHandler extends Handler {
-		ArrayList<Messenger> clients = new ArrayList<Messenger>();
-		private final WeakReference<JTTService> srv;
-		private final WeakReference<JTT> calc;
-
-		public JTTHandler(JTTService s) {
-			srv = new WeakReference<JTTService>(s);
-			calc = new WeakReference<JTT>(s.calculator);
-		}
-
-		@Override
-		public void handleMessage(Message msg) {
-			JTTService s = srv.get();
-			switch (msg.what) {
-			case MSG_SETTINGS_CHANGE:
-				s.notify = msg.getData().getBoolean("notify", s.notify);
-				JTTUtil.changeLocale(s, msg.getData().getString("locale"));
-				if (s.notify)
-					s.notify_helper(s.hour, s.quarter, s.part);
-				else
-					s.nm.cancel(APP_ID);
-				break;
-			case MSG_UPDATE_LOCATION:
-				String ll[] = msg.getData().getString("latlon").split(":");
-				calc.get().move(Float.parseFloat(ll[0]),
-						Float.parseFloat(ll[1]));
-				informClients(Message.obtain(null, MSG_INVALIDATE));
-				break;
-			case MSG_REGISTER_CLIENT:
-				try {
-					msg.replyTo.send(Message.obtain(null, MSG_HOUR, s.hour, s.part));
-					clients.add(msg.replyTo);
-				} catch (RemoteException e) {
-					Log.w(TAG, "Client registered but failed to get data");
-				}
-				break;
-			case MSG_UNREGISTER_CLIENT:
-				clients.remove(msg.replyTo);
-				break;
-			case MSG_STOP:
-				s.force_stop = true;
-				s.stopSelf();
-				break;
-			case MSG_TRANSITIONS:
-				try {
-					msg.replyTo.send(trans_msg(msg));
-				} catch (RemoteException e) {
-					Log.w(TAG, "Client requested transitions data but failed to get answer");
-				}
-				break;
-			default:
-				super.handleMessage(msg);
-				break;
-			}
-		}
-
-		public void informClients(Message msg) {
-			int i = clients.size();
-			while (i-- > 0)
-				try {
-					clients.get(i).send(msg);
-				} catch (RemoteException e) {
-					/*
-					 * The client is dead. Remove it from the list; we are going
-					 * through the list from back to front so this is safe to do
-					 * inside the loop.
-					 */
-					clients.remove(i);
-				}
-		}
-
-		private Message trans_msg(Message rq) {
-			Message resp = Message.obtain(null, MSG_TRANSITIONS);
-			Bundle b = new Bundle(rq.getData());
-			long jdn = b.getLong("jdn");
-			long[] tr = calc.get().computeTr(jdn);
-			b.putLong("sunrise", tr[0]);
-			b.putLong("sunset", tr[1]);
-			resp.setData(b);
-			return resp;
-		}
-	};
-
-	final JTTHandler handler = new JTTHandler(this);
-	final Messenger messenger = new Messenger(handler);
 
 	private String app_name;
 	private static final int bar_ids[] = {R.id.fraction1, R.id.fraction2, R.id.fraction3, R.id.fraction4};
