2e30d7ac9d4cb8bff7b681064764748bfb1693ea
Ilya Konovalov <aragaer@gmail.com>
2013-05-01 23:59:19 +0400

    Runtime resource factory class

---
+++ src/com/aragaer/jtt/resources/RuntimeResources.java
@@ -0,0 +1,53 @@
+package com.aragaer.jtt.resources;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+import android.content.Context;
+import android.util.Log;
+
+public class RuntimeResources {
+	protected final Context context;
+	private final static Map<Context, RuntimeResources> rr_singletons = new HashMap<Context, RuntimeResources>();
+	private final static Map<Class<?>, Object> singletons = new HashMap<Class<?>, Object>();
+
+	private RuntimeResources(final Context ctx) {
+		context = ctx;
+		rr_singletons.put(context, this);
+	}
+
+	public static RuntimeResources get(final Context c) {
+		final Context app_c = c.getApplicationContext();
+		return rr_singletons.containsKey(app_c)
+				? rr_singletons.get(app_c)
+				: new RuntimeResources(app_c);
+	}
+
+	@SuppressWarnings("unchecked")
+	public final <E> E getInstance(final Class<E> clazz) {
+		Constructor<E> ctor;
+		try {
+			ctor = clazz.getDeclaredConstructor(Context.class);
+		} catch (NoSuchMethodException e) {
+			Log.e("RR", "Failed to find constructor", e);
+			return null;
+		}
+		E res = (E) singletons.get(clazz);
+		if (res == null)
+			try {
+				res = ctor.newInstance(context);
+				singletons.put(clazz, res);
+			} catch (IllegalArgumentException e) {
+				Log.e("RR", "Failed to call constructor", e);
+			} catch (InstantiationException e) {
+				Log.e("RR", "Failed to call constructor", e);
+			} catch (IllegalAccessException e) {
+				Log.e("RR", "Failed to call constructor", e);
+			} catch (InvocationTargetException e) {
+				Log.e("RR", "Failed to call constructor", e);
+			}
+		return res;
+	}
+}
