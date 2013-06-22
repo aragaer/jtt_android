b4b5f3056bb368553a76b8eac8090679035c6844
Ilya Konovalov <aragaer@gmail.com>
2013-06-23 00:53:59 +0400

    Make lots of stuff static

--- src/com/aragaer/jtt/JTTWidgetProvider.java
+++ src/com/aragaer/jtt/JTTWidgetProvider.java
@@ -26,80 +26,106 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.RemoteViews;
 
+interface WidgetHandler {
+	void hour_changed(final int n);
+	void fill_rv(final RemoteViews rv, final Hour h);
+	void init(final Context c);
+}
+
 public class JTTWidgetProvider {
 	private static final String PKG_NAME = "com.aragaer.jtt";
 
-	private static abstract class JTTWidget extends AppWidgetProvider {
-		private final ComponentName name;
-		private final int granularity;
-		static private final Map<Class<JTTWidget>, Hour> last_update = new HashMap<Class<JTTWidget>, Hour>();
 
-		abstract protected void hour_changed(int n);
-		abstract protected void fill_rv(RemoteViews rv, Hour h);
-		abstract protected void init(Context c);
+	private static final class WidgetHolder {
+		final ComponentName cn;
+		Hour last_update;
+		final WidgetHandler handler;
+		final int granularity;
+
+		WidgetHolder(final Class cls, final WidgetHandler handler, int granularity) {
+			cn = new ComponentName(PKG_NAME, cls.getName());
+			this.handler = handler;
+			this.granularity = granularity;
+		}
+	}
+
+	private static abstract class JTTWidget extends AppWidgetProvider {
+		static private final Map<Class<?>, WidgetHolder> classes = new HashMap<Class<?>, WidgetHolder>();
 
 		static boolean inverse;
 
-		protected JTTWidget(final int frequency) {
-			name = new ComponentName(PKG_NAME, this.getClass().getName());
-			this.granularity = Hour.QUARTERS * Hour.QUARTER_PARTS / frequency;
+		private WidgetHolder myHolder() {
+			return classes.get(getClass());
+		}
+
+		protected JTTWidget(final int frequency, final Class<? extends WidgetHandler> handler_class) {
+			int granularity = Hour.QUARTERS * Hour.QUARTER_PARTS / frequency;
+
+			final Class<?> cls = getClass();
+			if (!classes.containsKey(cls))
+				try {
+					classes.put(cls, new WidgetHolder(cls, handler_class.newInstance(), granularity));
+				} catch (IllegalAccessException e) {
+					Log.e("JTT WIDGET", "Failed to instantiate handler", e);
+				} catch (InstantiationException e) {
+					Log.e("JTT WIDGET", "Failed to instantiate handler", e);
+				}
 		}
 
 		public void onReceive(Context c, Intent i) {
 			final String action = i.getAction();
-			init(c);
+			myHolder().handler.init(c);
 			inverse = PreferenceManager.getDefaultSharedPreferences(c)
 					.getBoolean("jtt_widget_text_invert", false);
 			if (action.equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE))
 				update(c, i);
 			else if (action.equals(Clockwork.ACTION_JTT_TICK))
-				tick(c, i);
+				tick(c, i, getClass());
 			else if (action.equals(Settings.JTT_SETTINGS_CHANGED)) {
 				inverse = i.getBooleanExtra("inverse", inverse);
-				draw(c, null, last_update.get(getClass()));
+				draw(c, null, getClass());
 			} else
 				Log.d("Widgets", "Got action "+action);
 		}
 
-		private void tick(Context c, Intent i) {
+		private static void tick(Context c, Intent i, Class<?> cls) {
 			int n = i.getIntExtra("hour", 0);
 			int wrapped = i.getIntExtra("jtt", 0);
-			Hour prev = last_update.get(getClass());
+			final WidgetHolder holder = classes.get(cls);
+			Hour prev = holder.last_update;
 			if (prev == null) {
-				wrapped -= wrapped % granularity;
-				prev = Hour.fromWrapped(wrapped, null);
-				last_update.put((Class<JTTWidget>) getClass(), prev);
-				hour_changed(n);
+				wrapped -= wrapped % holder.granularity;
+				holder.last_update = prev = Hour.fromWrapped(wrapped, null);
+				holder.handler.hour_changed(n);
 			} else {
 				if (prev.num != n)
-					hour_changed(n);
-				if (!prev.compareAndUpdate(wrapped, granularity))
+					holder.handler.hour_changed(n);
+				if (!prev.compareAndUpdate(wrapped, holder.granularity))
 					return; // do nothing
 			}
-			draw(c, null, prev);
+			draw(c, null, cls);
 		}
 
 		private void update(Context c, Intent i) {
 			int[] ids = i.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
-			Hour h = last_update.get(getClass());
-			if (h == null)
-				return;
-			draw(c, ids, h);
+			draw(c, ids, getClass());
 		}
 
-		private void draw(Context c, int[] ids, Hour h) {
+		private static void draw(Context c, int[] ids, Class<?> cls) {
+			final WidgetHolder holder = classes.get(cls);
 			final AppWidgetManager awm = AppWidgetManager.getInstance(c.getApplicationContext());
 			if (ids == null)
-				ids = awm.getAppWidgetIds(name);
+				ids = awm.getAppWidgetIds(holder.cn);
 			if (ids.length == 0)
 				return;
 
+			Hour h = holder.last_update;
 			RemoteViews rv;
 			if (h == null)
 				rv = new RemoteViews(PKG_NAME, R.layout.widget_loading);
 			else {
 				rv = new RemoteViews(PKG_NAME, inverse ? R.layout.widget_inverse : R.layout.widget);
-				fill_rv(rv, h);
+				holder.handler.fill_rv(rv, h);
 			}
 			PendingIntent pendingIntent = PendingIntent.getActivity(c, 0, new Intent(c, JTTMainActivity.class), 0);
 			rv.setOnClickPendingIntent(R.id.clock, pendingIntent);
@@ -111,89 +137,95 @@ public class JTTWidgetProvider {
 
 	/* Widget showing only 1 hour */
 	public static class Widget1 extends JTTWidget {
-		/* Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG | Paint.DITHER_FLAG == 0x07 */
-		private final Paint p1 = new Paint(0x07), p2 = new Paint(p1);
-		private final Bitmap bmp = Bitmap.createBitmap(80, 80, Bitmap.Config.ARGB_4444);
-		private final Canvas c = new Canvas(bmp);
-		private final Path path1 = new Path(), path2 = new Path();
-		private final RectF outer = new RectF(10, 10, 70, 70), inner = new RectF(20, 20, 60, 60);
-		static boolean initialized = false;
-
 		public Widget1() {
-			super(20);
-
-			p1.setStyle(Paint.Style.FILL);
-			p1.setColor(Color.TRANSPARENT);
-			p2.setStyle(Paint.Style.FILL);
-			p2.setColorFilter(new LightingColorFilter(0xFFFFFFFF, 0xFFCCCCCC));
-
-			path1.addCircle(40, 40, 20, Path.Direction.CW);
-			path1.addCircle(40, 40, 30, Path.Direction.CCW);
-		}
-
-		protected void hour_changed(int n) { }
-
-		private static final float QUARTER_ANGLE = 360f / Hour.QUARTERS,
-				PART_ANGLE = QUARTER_ANGLE / Hour.QUARTER_PARTS;
-		protected void fill_rv(RemoteViews rv, Hour h) {
-			bmp.eraseColor(Color.TRANSPARENT);
-
-			c.drawPath(path1, p1);
-
-			final float angle = QUARTER_ANGLE * h.quarter + PART_ANGLE * h.quarter_parts;
-
-			path2.reset();
-			path2.addArc(inner, angle - 90, -angle);
-			path2.arcTo(outer, -90, angle);
-			c.drawPath(path2, p2);
-
-			rv.setImageViewBitmap(R.id.clock, bmp);
-			rv.setTextViewText(R.id.glyph, Hour.Glyphs[h.num]);
-		}
-
-		protected void init(Context c) {
-			if (initialized)
-				return;
-
-			p1.setShadowLayer(10f, 0, 0, Color.parseColor(c.getString(R.color.night)));
-			p2.setColor(Color.parseColor(c.getString(R.color.fill)));
-			initialized = true;
+			super(20, WidgetHandler1.class);
 		}
 	}
 
 	/* Widget showing 12 hours */
 	public static class Widget12 extends JTTWidget {
-		static StringResources sr;
-		static WadokeiDraw wd;
-		static int size;
-		private static Bitmap bmp;
-
 		public Widget12() {
-			super(8);
+			super(8, WidgetHandler12.class);
 		}
+	}
+}
 
-		protected void hour_changed(int n) {
-			wd.prepare_glyphs(n);
-		}
+class WidgetHandler1 implements WidgetHandler {
+	/* Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG | Paint.DITHER_FLAG == 0x07 */
+	private static final Paint p1 = new Paint(0x07), p2 = new Paint(p1);
+	private static final Bitmap bmp = Bitmap.createBitmap(80, 80, Bitmap.Config.ARGB_4444);
+	private static final Canvas c = new Canvas(bmp);
+	private static final Path path1 = new Path(), path2 = new Path();
+	private static final RectF outer = new RectF(10, 10, 70, 70), inner = new RectF(20, 20, 60, 60);
+	static boolean initialized = false;
 
-		protected void fill_rv(RemoteViews rv, Hour h) {
-			bmp.eraseColor(Color.TRANSPARENT);
-			wd.draw_dial(h, new Canvas(bmp));
+	static {
+		p1.setStyle(Paint.Style.FILL);
+		p1.setColor(Color.TRANSPARENT);
+		p2.setStyle(Paint.Style.FILL);
+		p2.setColorFilter(new LightingColorFilter(0xFFFFFFFF, 0xFFCCCCCC));
 
-			rv.setImageViewBitmap(R.id.clock, bmp);
-			rv.setFloat(R.id.glyph, "setTextSize", size / 10);
-			rv.setTextViewText(R.id.glyph, sr.getHour(h.num));
-		}
+		path1.addCircle(40, 40, 20, Path.Direction.CW);
+		path1.addCircle(40, 40, 30, Path.Direction.CCW);
+	}
 
-		protected void init(Context c) {
-			if (sr != null)
-				return;
-			sr = RuntimeResources.get(c).getInstance(StringResources.class);
-			wd = new WadokeiDraw(c);
-			size = Math.round(110 * c.getResources().getDisplayMetrics().density);
+	public void hour_changed(final int n) { }
 
-			bmp = Bitmap.createBitmap(size * 2, size * 2, Bitmap.Config.ARGB_4444);
-			wd.set_dial_size(size);
-		}
+	private static final float QUARTER_ANGLE = 360f / Hour.QUARTERS,
+			PART_ANGLE = QUARTER_ANGLE / Hour.QUARTER_PARTS;
+	public void fill_rv(final RemoteViews rv, final Hour h) {
+		bmp.eraseColor(Color.TRANSPARENT);
+
+		c.drawPath(path1, p1);
+
+		final float angle = QUARTER_ANGLE * h.quarter + PART_ANGLE * h.quarter_parts;
+
+		path2.reset();
+		path2.addArc(inner, angle - 90, -angle);
+		path2.arcTo(outer, -90, angle);
+		c.drawPath(path2, p2);
+
+		rv.setImageViewBitmap(R.id.clock, bmp);
+		rv.setTextViewText(R.id.glyph, Hour.Glyphs[h.num]);
+	}
+
+	public void init(final Context c) {
+		if (initialized)
+			return;
+
+		p1.setShadowLayer(10f, 0, 0, Color.parseColor(c.getString(R.color.night)));
+		p2.setColor(Color.parseColor(c.getString(R.color.fill)));
+		initialized = true;
+	}
+}
+
+class WidgetHandler12 implements WidgetHandler {
+	static StringResources sr;
+	static WadokeiDraw wd;
+	static int size;
+	private static Bitmap bmp;
+
+	public void hour_changed(final int n) {
+		wd.prepare_glyphs(n);
+	}
+
+	public void fill_rv(final RemoteViews rv, final Hour h) {
+		bmp.eraseColor(Color.TRANSPARENT);
+		wd.draw_dial(h, new Canvas(bmp));
+
+		rv.setImageViewBitmap(R.id.clock, bmp);
+		rv.setFloat(R.id.glyph, "setTextSize", size / 10);
+		rv.setTextViewText(R.id.glyph, sr.getHour(h.num));
+	}
+
+	public void init(final Context c) {
+		if (sr != null)
+			return;
+		sr = RuntimeResources.get(c).getInstance(StringResources.class);
+		wd = new WadokeiDraw(c);
+		size = Math.round(110 * c.getResources().getDisplayMetrics().density);
+
+		bmp = Bitmap.createBitmap(size * 2, size * 2, Bitmap.Config.ARGB_4444);
+		wd.set_dial_size(size);
 	}
 }
