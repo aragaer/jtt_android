16cb2ac4526408d72d45bd952c6eca83f47636e7
aragaer <aragaer@gmail.com>
2014-11-16 15:58:02 +0300

    Fix indentation in JttService

--- src/com/aragaer/jtt/JttService.java
+++ src/com/aragaer/jtt/JttService.java
@@ -1,4 +1,5 @@
 package com.aragaer.jtt;
+// vim: et ts=4 sts=4 sw=4
 
 import com.aragaer.jtt.clockwork.AndroidClock;
 import com.aragaer.jtt.clockwork.Clock;
@@ -12,55 +13,55 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class JttService extends Service implements SharedPreferences.OnSharedPreferenceChangeListener {
-	private static final String TAG = "JTT_SERVICE";
-	private JttStatus status_notify;
-	private final Clock clock;
-	private TimeDateChangeReceiver timeDateChangeReceiver;
+    private static final String TAG = "JTT_SERVICE";
+    private JttStatus status_notify;
+    private final Clock clock;
+    private TimeDateChangeReceiver timeDateChangeReceiver;
 
-	public JttService() {
-		clock = AndroidClock.createFromContext(this);
-	}
+    public JttService() {
+        clock = AndroidClock.createFromContext(this);
+    }
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		return null;
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		Log.i(TAG, "Service starting");
-		clock.adjust();
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, "Service starting");
+        clock.adjust();
 
-		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
-		pref.registerOnSharedPreferenceChangeListener(this);
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        pref.registerOnSharedPreferenceChangeListener(this);
 
-		toggle_notify(pref.getBoolean("jtt_notify", true));
+        toggle_notify(pref.getBoolean("jtt_notify", true));
 
-		timeDateChangeReceiver = new TimeDateChangeReceiver(clock);
-		timeDateChangeReceiver.register(this);
-	}
+        timeDateChangeReceiver = new TimeDateChangeReceiver(clock);
+        timeDateChangeReceiver.register(this);
+    }
 
-	private void toggle_notify(final boolean notify) {
-		if (status_notify == null) {
-			if (notify)
-				status_notify = new JttStatus(this);
-		} else {
-			if (!notify) {
-				status_notify.release();
-				status_notify = null;
-			}
-		}
-	}
+    private void toggle_notify(final boolean notify) {
+        if (status_notify == null) {
+            if (notify)
+                status_notify = new JttStatus(this);
+        } else {
+            if (!notify) {
+                status_notify.release();
+                status_notify = null;
+            }
+        }
+    }
 
-	public void onSharedPreferenceChanged(SharedPreferences pref, String key) {
-		if (key.equals(Settings.PREF_NOTIFY))
-			toggle_notify(pref.getBoolean("jtt_notify", true));
-		else if (key.equals(Settings.PREF_LOCATION))
-			clock.adjust();
-		else if (key.equals(Settings.PREF_WIDGET)
-				|| key.equals(Settings.PREF_LOCALE)
-				|| key.equals(Settings.PREF_HNAME))
-			WidgetProvider.draw_all(this);
-	}
+    public void onSharedPreferenceChanged(SharedPreferences pref, String key) {
+        if (key.equals(Settings.PREF_NOTIFY))
+            toggle_notify(pref.getBoolean("jtt_notify", true));
+        else if (key.equals(Settings.PREF_LOCATION))
+            clock.adjust();
+        else if (key.equals(Settings.PREF_WIDGET)
+                || key.equals(Settings.PREF_LOCALE)
+                || key.equals(Settings.PREF_HNAME))
+            WidgetProvider.draw_all(this);
+    }
 }
