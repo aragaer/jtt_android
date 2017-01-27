e492351661e78f9b1515521eb5999ecb2eccbc8b
aragaer <aragaer@gmail.com>
2017-01-28 00:02:37 +0300

    Extract actual Ticker to yet another separate class

--- src/com/aragaer/jtt/android/AndroidTicker.java
+++ src/com/aragaer/jtt/android/AndroidTicker.java
@@ -2,26 +2,22 @@
 // vim: et ts=4 sts=4 sw=4 syntax=java
 package com.aragaer.jtt.android;
 
-import com.aragaer.jtt.core.*;
+import com.aragaer.jtt.core.Clockwork;
 
-import android.app.*;
-import android.content.*;
-import android.util.Log;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 
 
-public class AndroidTicker extends IntentService {
+public class AndroidTicker {
     public static final String ACTION_JTT_TICK = "com.aragaer.jtt.action.TICK";
-    private static final Intent TickAction = new Intent(ACTION_JTT_TICK);
-
-    public AndroidTicker() {
-        super("CLOCKWORK");
-    }
 
     public static void schedule(final Context context) {
         long now = System.currentTimeMillis();
         Clockwork starter = new Clockwork(context, now);
 
-        Intent TickActionInternal = new Intent(context, AndroidTicker.class)
+        Intent TickActionInternal = new Intent(context, Ticker.class)
             .putExtra("intervals", starter.data);
 
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
@@ -31,28 +27,6 @@ public class AndroidTicker extends IntentService {
 
     public static void unschedule(final Context context) {
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        am.cancel(PendingIntent.getService(context, 0, new Intent(context, AndroidTicker.class), 0));
-    }
-
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        ThreeIntervals intervals = (ThreeIntervals) intent.getSerializableExtra("intervals");
-        long now = System.currentTimeMillis();
-        Hour hour = Hour.fromInterval(intervals.getMiddleInterval(), now, null);
-
-        if (intervals.surrounds(now)) {
-            Intent TickAction = new Intent(ACTION_JTT_TICK)
-                .putExtra("intervals", intervals)
-                .putExtra("hour", hour.num)
-                .putExtra("jtt", hour.wrapped);
-            sendStickyBroadcast(TickAction);
-        } else
-            try {
-                schedule(this);
-            } catch (IllegalStateException e) {
-                Log.i("JTT CLOCKWORK", "Transition passed while service is not running, ignore");
-            }
-
-        stopSelf();
+        am.cancel(PendingIntent.getService(context, 0, new Intent(context, Ticker.class), 0));
     }
 }
---
+++ src/com/aragaer/jtt/android/Ticker.java
@@ -0,0 +1,39 @@
+// -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; -*-
+// vim: et ts=4 sts=4 sw=4 syntax=java
+package com.aragaer.jtt.android;
+
+import com.aragaer.jtt.core.Hour;
+import com.aragaer.jtt.core.ThreeIntervals;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+public class Ticker extends IntentService {
+
+    public Ticker() {
+	super("CLOCKWORK");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+	ThreeIntervals intervals = (ThreeIntervals) intent.getSerializableExtra("intervals");
+	long now = System.currentTimeMillis();
+
+	if (intervals.surrounds(now)) {
+	    Hour hour = Hour.fromInterval(intervals.getMiddleInterval(), now, null);
+	    Intent TickAction = new Intent(AndroidTicker.ACTION_JTT_TICK)
+		.putExtra("intervals", intervals)
+		.putExtra("hour", hour.num)
+		.putExtra("jtt", hour.wrapped);
+	    sendStickyBroadcast(TickAction);
+	} else
+	    try {
+		AndroidTicker.schedule(this);
+	    } catch (IllegalStateException e) {
+		Log.i("JTT CLOCKWORK", "Transition passed while service is not running, ignore");
+	    }
+
+	stopSelf();
+    }
+}
