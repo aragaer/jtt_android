03939e47fb447f62ddf6f759413ba072e2897922
aragaer <aragaer@gmail.com>
2017-01-27 22:55:28 +0300

    Fix BootReceiver warning

---
+++ src/com/aragaer/jtt/android/BootReceiver.java
@@ -0,0 +1,19 @@
+// -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; -*-
+// vim: et ts=4 sts=4 sw=4 syntax=java
+package com.aragaer.jtt.android;
+
+import com.aragaer.jtt.JttService;
+
+import android.content.*;
+import android.preference.PreferenceManager;
+
+public class BootReceiver extends BroadcastReceiver {
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (action == null || !action.equals(Intent.ACTION_BOOT_COMPLETED))
+            return;
+        if (PreferenceManager.getDefaultSharedPreferences(context)
+            .getBoolean("jtt_bootup", true))
+            context.startService(new Intent(context, JttService.class));
+    }
+}
