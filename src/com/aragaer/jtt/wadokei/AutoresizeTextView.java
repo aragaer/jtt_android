eee79c5404d8b2e35bcaa19e6c51f26fb9a1cda1
aragaer <aragaer@gmail.com>
2017-01-29 20:29:50 +0300

    Fix AutoresizeTextView formatting

--- src/com/aragaer/jtt/wadokei/AutoresizeTextView.java
+++ src/com/aragaer/jtt/wadokei/AutoresizeTextView.java
@@ -1,3 +1,5 @@
+// -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; -*-
+// vim: et ts=4 sts=4 sw=4 syntax=java
 package com.aragaer.jtt.wadokei;
 
 import android.content.Context;
@@ -5,18 +7,18 @@ import android.util.TypedValue;
 import android.widget.TextView;
 
 public class AutoresizeTextView extends TextView {
-	public AutoresizeTextView(Context context) {
-		super(context);
-	}
+    public AutoresizeTextView(Context context) {
+        super(context);
+    }
 
-	protected void onMeasure(int wms, int hms) {
-		super.onMeasure(wms, hms);
-		float measured_width = getPaint().measureText(getText().toString());
-		int allowed = MeasureSpec.getSize(wms);
-		if (measured_width > allowed) {
-			setTextSize(TypedValue.COMPLEX_UNIT_PX, (getTextSize() * allowed)
-					/ measured_width);
-			super.onMeasure(wms, hms);
-		}
-	}
+    protected void onMeasure(int wms, int hms) {
+        super.onMeasure(wms, hms);
+        float measured_width = getPaint().measureText(getText().toString());
+        int allowed = MeasureSpec.getSize(wms);
+        if (measured_width > allowed) {
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, (getTextSize() * allowed)
+                        / measured_width);
+            super.onMeasure(wms, hms);
+        }
+    }
 }
