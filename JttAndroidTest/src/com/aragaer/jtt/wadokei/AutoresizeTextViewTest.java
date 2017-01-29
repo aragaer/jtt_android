6b985ceaeacd2322435deb6ae239bca6add38cba
aragaer <aragaer@gmail.com>
2017-01-29 03:22:23 +0300

    Test that AutoresizeTextView doesn't resize if there is enough space

--- JttAndroidTest/src/com/aragaer/jtt/wadokei/AutoresizeTextViewTest.java
+++ JttAndroidTest/src/com/aragaer/jtt/wadokei/AutoresizeTextViewTest.java
@@ -1,14 +1,29 @@
+// -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; -*-
+// vim: et ts=4 sts=4 sw=4 syntax=java
 package com.aragaer.jtt.wadokei;
 
 import android.test.AndroidTestCase;
 import android.widget.TextView;
 
+
 public class AutoresizeTextViewTest extends AndroidTestCase {
-	public void testChangeTextSize() {
-		TextView view = new AutoresizeTextView(getContext());
-		view.setTextSize(42);
-		view.setText("test");
-		view.measure(100, 0);
-		assertTrue(view.getPaint().measureText("test") <= 100);
-	}
+
+    public void testChangeTextSize() {
+        float scale = getContext().getResources().getDisplayMetrics().density;
+        TextView view = new AutoresizeTextView(getContext());
+        view.setTextSize(42);
+        view.setText("test");
+        view.measure(100, 0);
+        assertTrue(view.getPaint().measureText("test") <= 100);
+        assertTrue(view.getTextSize() <= 42*scale);
+    }
+
+    public void testKeepSizeIfFits() {
+        float scale = getContext().getResources().getDisplayMetrics().density;
+        TextView view = new AutoresizeTextView(getContext());
+        view.setTextSize(12);
+        view.setText("test");
+        view.measure(100, 0);
+        assertEquals(view.getTextSize(), 12*scale);
+    }
 }
