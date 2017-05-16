abb58cd73647fba84b102148c2ff9ce95fa99448
aragaer <aragaer@gmail.com>
2017-05-17 00:36:01 +0300

    Fix formatting in StringResources

--- src/main/java/com/aragaer/jtt/resources/StringResources.java
+++ src/main/java/com/aragaer/jtt/resources/StringResources.java
@@ -1,3 +1,5 @@
+// -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; -*-
+// vim: et ts=4 sts=4 sw=4 syntax=java
 package com.aragaer.jtt.resources;
 
 import java.text.DateFormat;
@@ -18,121 +20,121 @@ import android.content.res.Resources;
 import android.preference.PreferenceManager;
 
 public class StringResources implements
-		SharedPreferences.OnSharedPreferenceChangeListener {
-	public static final int TYPE_HOUR_NAME = 0x1;
-	public static final int TYPE_TIME_FORMAT = 0x2;
-	private int change_pending;
+                                 SharedPreferences.OnSharedPreferenceChangeListener {
+    public static final int TYPE_HOUR_NAME = 0x1;
+    public static final int TYPE_TIME_FORMAT = 0x2;
+    private int change_pending;
 
-	private final BroadcastReceiver TZChangeReceiver = new BroadcastReceiver() {
-		public void onReceive(Context context, Intent intent) {
-			if (intent.getAction().equals(Intent.ACTION_TIMEZONE_CHANGED)) {
-				df = android.text.format.DateFormat.getTimeFormat(c);
-				change_pending |= TYPE_TIME_FORMAT;
-				notifyChange();
-			}
-		}
-	};
+    private final BroadcastReceiver TZChangeReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals(Intent.ACTION_TIMEZONE_CHANGED)) {
+                    df = android.text.format.DateFormat.getTimeFormat(c);
+                    change_pending |= TYPE_TIME_FORMAT;
+                    notifyChange();
+                }
+            }
+        };
 
-	private static final IntentFilter filter = new IntentFilter(Intent.ACTION_TIMEZONE_CHANGED);
-	private final Context c;
-	private final Resources r;
-	private String Hours[], HrOf[], Quarters[];
-	private DateFormat df;
-	private int hour_name_option;
+    private static final IntentFilter filter = new IntentFilter(Intent.ACTION_TIMEZONE_CHANGED);
+    private final Context c;
+    private final Resources r;
+    private String Hours[], HrOf[], Quarters[];
+    private DateFormat df;
+    private int hour_name_option;
 
-	protected StringResources(final Context context) {
-		c = context;
-		r = new Resources(c.getAssets(), null, null);
-		final SharedPreferences pref = PreferenceManager
-				.getDefaultSharedPreferences(c);
-		hour_name_option = Integer.parseInt(pref.getString(Settings.PREF_HNAME, "0"));
-		setLocale(pref.getString(Settings.PREF_LOCALE, ""));
-		pref.registerOnSharedPreferenceChangeListener(this);
-	}
+    protected StringResources(final Context context) {
+        c = context;
+        r = new Resources(c.getAssets(), null, null);
+        final SharedPreferences pref = PreferenceManager
+            .getDefaultSharedPreferences(c);
+        hour_name_option = Integer.parseInt(pref.getString(Settings.PREF_HNAME, "0"));
+        setLocale(pref.getString(Settings.PREF_LOCALE, ""));
+        pref.registerOnSharedPreferenceChangeListener(this);
+    }
 
-	private static void setLocaleToResources(final String l, final Resources r) {
-		final Locale locale = l.length() == 0
-				? Resources.getSystem().getConfiguration().locale
-				: new Locale(l);
-		final Configuration config = r.getConfiguration();
-		config.locale = locale;
-		r.updateConfiguration(config, null);
-	}
+    private static void setLocaleToResources(final String l, final Resources r) {
+        final Locale locale = l.length() == 0
+            ? Resources.getSystem().getConfiguration().locale
+            : new Locale(l);
+        final Configuration config = r.getConfiguration();
+        config.locale = locale;
+        r.updateConfiguration(config, null);
+    }
 
-	public static void setLocaleToContext(Context context) {
-		final SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
-		final String lang = pref.getString(Settings.PREF_LOCALE, "");
-		setLocaleToResources(lang, context.getResources());
-	}
+    public static void setLocaleToContext(Context context) {
+        final SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+        final String lang = pref.getString(Settings.PREF_LOCALE, "");
+        setLocaleToResources(lang, context.getResources());
+    }
 
-	private synchronized void setLocale(final String l) {
-		setLocaleToResources(l, c.getApplicationContext().getResources());
-		load_hour_names();
-		df = android.text.format.DateFormat.getTimeFormat(c);
-		change_pending |= TYPE_TIME_FORMAT;
-	}
+    private synchronized void setLocale(final String l) {
+        setLocaleToResources(l, c.getApplicationContext().getResources());
+        load_hour_names();
+        df = android.text.format.DateFormat.getTimeFormat(c);
+        change_pending |= TYPE_TIME_FORMAT;
+    }
 
-	public void onSharedPreferenceChanged(SharedPreferences pref, String key) {
-		if (key.equals(Settings.PREF_LOCALE))
-			setLocale(pref.getString(key, ""));
-		else if (key.equals(Settings.PREF_HNAME)) {
-			hour_name_option = Integer.parseInt(pref.getString(key, "0"));
-			load_hour_names();
-		}
-		notifyChange();
-	}
+    public void onSharedPreferenceChanged(SharedPreferences pref, String key) {
+        if (key.equals(Settings.PREF_LOCALE))
+            setLocale(pref.getString(key, ""));
+        else if (key.equals(Settings.PREF_HNAME)) {
+            hour_name_option = Integer.parseInt(pref.getString(key, "0"));
+            load_hour_names();
+        }
+        notifyChange();
+    }
 
-	public String getHour(final int num) {
-		return Hours[num];
-	}
+    public String getHour(final int num) {
+        return Hours[num];
+    }
 
-	public String getHrOf(final int num) {
-		return HrOf[num];
-	}
+    public String getHrOf(final int num) {
+        return HrOf[num];
+    }
 
-	public String getQuarter(final int q) {
-		return Quarters[q];
-	}
+    public String getQuarter(final int q) {
+        return Quarters[q];
+    }
 
-	public interface StringResourceChangeListener {
-		public void onStringResourcesChanged(final int changes);
-	}
+    public interface StringResourceChangeListener {
+        public void onStringResourcesChanged(final int changes);
+    }
 
-	private final Map<StringResourceChangeListener, Integer> listeners = new HashMap<StringResources.StringResourceChangeListener, Integer>();
+    private final Map<StringResourceChangeListener, Integer> listeners = new HashMap<StringResources.StringResourceChangeListener, Integer>();
 
-	public synchronized void registerStringResourceChangeListener(
-			final StringResourceChangeListener listener, final int changeMask) {
-		if (listeners.size() == 0)
-			c.registerReceiver(TZChangeReceiver, filter);
-		listeners.put(listener, changeMask);
-	}
+    public synchronized void registerStringResourceChangeListener(
+                                                                  final StringResourceChangeListener listener, final int changeMask) {
+        if (listeners.size() == 0)
+            c.registerReceiver(TZChangeReceiver, filter);
+        listeners.put(listener, changeMask);
+    }
 
-	public synchronized void unregisterStringResourceChangeListener(
-			final StringResourceChangeListener listener) {
-		listeners.remove(listener);
-		if (listeners.size() == 0)
-			c.unregisterReceiver(TZChangeReceiver);
-	}
+    public synchronized void unregisterStringResourceChangeListener(
+                                                                    final StringResourceChangeListener listener) {
+        listeners.remove(listener);
+        if (listeners.size() == 0)
+            c.unregisterReceiver(TZChangeReceiver);
+    }
 
-	private synchronized void notifyChange() {
-		for (StringResourceChangeListener listener : listeners.keySet())
-			if ((listeners.get(listener) & change_pending) != 0)
-				listener.onStringResourcesChanged(change_pending);
-		change_pending = 0;
-	}
+    private synchronized void notifyChange() {
+        for (StringResourceChangeListener listener : listeners.keySet())
+            if ((listeners.get(listener) & change_pending) != 0)
+                listener.onStringResourcesChanged(change_pending);
+        change_pending = 0;
+    }
 
-	private static final int hnh[] = { R.array.hour, R.array.romaji_hour, R.array.hiragana_hour };
-	private static final int hnhof[] = { R.array.hour_of, R.array.romaji_hour_of, R.array.hiragana_hour_of };
-	private static final int q[] = { R.array.quarter, R.array.romaji_quarter, R.array.hiragana_quarter };
+    private static final int hnh[] = { R.array.hour, R.array.romaji_hour, R.array.hiragana_hour };
+    private static final int hnhof[] = { R.array.hour_of, R.array.romaji_hour_of, R.array.hiragana_hour_of };
+    private static final int q[] = { R.array.quarter, R.array.romaji_quarter, R.array.hiragana_quarter };
 
-	private void load_hour_names() {
-		HrOf = r.getStringArray(hnhof[hour_name_option]);
-		Hours = r.getStringArray(hnh[hour_name_option]);
-		Quarters = r.getStringArray(q[hour_name_option]);
-		change_pending |= TYPE_HOUR_NAME;
-	}
+    private void load_hour_names() {
+        HrOf = r.getStringArray(hnhof[hour_name_option]);
+        Hours = r.getStringArray(hnh[hour_name_option]);
+        Quarters = r.getStringArray(q[hour_name_option]);
+        change_pending |= TYPE_HOUR_NAME;
+    }
 
-	public String format_time(final long timestamp) {
-		return df.format(timestamp);
-	}
+    public String format_time(final long timestamp) {
+        return df.format(timestamp);
+    }
 }
