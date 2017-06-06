3269d9cbcee4187fc5ff314fa3961f0e0e907d72
aragaer <aragaer@gmail.com>
2017-06-06 10:13:57 +0300

    Fix indentation in InputFilterMinMaxTest

--- src/test/java/com/aragaer/jtt/ui/android/InputFilterMinMaxTest.java
+++ src/test/java/com/aragaer/jtt/ui/android/InputFilterMinMaxTest.java
@@ -16,82 +16,82 @@ public class InputFilterMinMaxTest {
     private InputFilterMinMax filter;
 
     @Before public void setUp() {
-	filter = new InputFilterMinMax(-100, 100);
+        filter = new InputFilterMinMax(-100, 100);
     }
 
     @Test public void shouldAcceptDeletion() {
-	FakeSpanned dest = new FakeSpanned("");
-	assertNull(filter.filter("", 0, 0, dest, 0, 0));
-	assertNull(filter.filter("word", 0, 0, dest, 0, 0));
-	assertNull(filter.filter("word", 1, 1, dest, 0, 0));
+        FakeSpanned dest = new FakeSpanned("");
+        assertNull(filter.filter("", 0, 0, dest, 0, 0));
+        assertNull(filter.filter("word", 0, 0, dest, 0, 0));
+        assertNull(filter.filter("word", 1, 1, dest, 0, 0));
     }
 
     @Test public void shouldAcceptSmallNumbers() {
-	assertNull(filter.filter("1", 0, 1, new FakeSpanned(""), 0, 0));
-	assertNull(filter.filter("1", 0, 1, new FakeSpanned("0"), 0, 0));
-	assertNull(filter.filter("1", 0, 1, new FakeSpanned("0"), 0, 1));
-	assertNull(filter.filter("1", 0, 1, new FakeSpanned("0"), 1, 1));
-	assertNull(filter.filter("1", 0, 1, new FakeSpanned("22"), 0, 1));
-	assertNull(filter.filter("1", 0, 1, new FakeSpanned("22"), 1, 2));
-	assertNull(filter.filter("1", 0, 1, new FakeSpanned("222"), 0, 2));
-	assertNull(filter.filter("1", 0, 1, new FakeSpanned("222"), 1, 3));
-	assertNull(filter.filter(".", 0, 1, new FakeSpanned("1"), 0, 0));
-	assertNull(filter.filter(".", 0, 1, new FakeSpanned("01"), 1, 1));
+        assertNull(filter.filter("1", 0, 1, new FakeSpanned(""), 0, 0));
+        assertNull(filter.filter("1", 0, 1, new FakeSpanned("0"), 0, 0));
+        assertNull(filter.filter("1", 0, 1, new FakeSpanned("0"), 0, 1));
+        assertNull(filter.filter("1", 0, 1, new FakeSpanned("0"), 1, 1));
+        assertNull(filter.filter("1", 0, 1, new FakeSpanned("22"), 0, 1));
+        assertNull(filter.filter("1", 0, 1, new FakeSpanned("22"), 1, 2));
+        assertNull(filter.filter("1", 0, 1, new FakeSpanned("222"), 0, 2));
+        assertNull(filter.filter("1", 0, 1, new FakeSpanned("222"), 1, 3));
+        assertNull(filter.filter(".", 0, 1, new FakeSpanned("1"), 0, 0));
+        assertNull(filter.filter(".", 0, 1, new FakeSpanned("01"), 1, 1));
     }
 
     @Test public void shouldAcceptSingleMinus() {
-	assertNull(filter.filter("-", 0, 1, new FakeSpanned(""), 0, 0));
+        assertNull(filter.filter("-", 0, 1, new FakeSpanned(""), 0, 0));
     }
 
     @Test public void shouldRejectLargeNumbers() {
-	assertEquals(filter.filter("200", 0, 3, new FakeSpanned("50"), 0, 2), "50");
-	assertEquals(filter.filter("200", 0, 3, new FakeSpanned(""), 0, 0), "");
-	assertEquals(filter.filter("20", 0, 2, new FakeSpanned("0"), 0, 0), "");
-	assertEquals(filter.filter("22", 0, 2, new FakeSpanned("3"), 1, 1), "");
-	assertEquals(filter.filter("1234", 1, 4, new FakeSpanned("78"), 0, 2), "78");
+        assertEquals(filter.filter("200", 0, 3, new FakeSpanned("50"), 0, 2), "50");
+        assertEquals(filter.filter("200", 0, 3, new FakeSpanned(""), 0, 0), "");
+        assertEquals(filter.filter("20", 0, 2, new FakeSpanned("0"), 0, 0), "");
+        assertEquals(filter.filter("22", 0, 2, new FakeSpanned("3"), 1, 1), "");
+        assertEquals(filter.filter("1234", 1, 4, new FakeSpanned("78"), 0, 2), "78");
     }
 
     @Test public void shouldRejectNonNumbers() {
-	assertEquals(filter.filter("x", 0, 1, new FakeSpanned(""), 0, 0), "");
-	assertEquals(filter.filter("x", 0, 1, new FakeSpanned("10"), 0, 2), "10");
-	assertEquals(filter.filter(".", 0, 1, new FakeSpanned("0.1"), 0, 0), "");
-	assertEquals(filter.filter("-", 0, 1, new FakeSpanned("12"), 1, 1), "");
-	assertEquals(filter.filter("1", 0, 1, new FakeSpanned("-12"), 0, 0), "");
+        assertEquals(filter.filter("x", 0, 1, new FakeSpanned(""), 0, 0), "");
+        assertEquals(filter.filter("x", 0, 1, new FakeSpanned("10"), 0, 2), "10");
+        assertEquals(filter.filter(".", 0, 1, new FakeSpanned("0.1"), 0, 0), "");
+        assertEquals(filter.filter("-", 0, 1, new FakeSpanned("12"), 1, 1), "");
+        assertEquals(filter.filter("1", 0, 1, new FakeSpanned("-12"), 0, 0), "");
     }
 
     private static class FakeSpanned implements Spanned {
-	private CharSequence data;
+        private CharSequence data;
 
-	FakeSpanned(CharSequence data) {
-	    this.data = data;
-	}
+        FakeSpanned(CharSequence data) {
+            this.data = data;
+        }
 
-	@Override public int getSpanStart(Object tag) {
-	    return 0;
-	}
-	@Override public int getSpanEnd(Object tag) {
-	    return 0;
-	}
-	@Override public int getSpanFlags(Object tag) {
-	    return 0;
-	}
-	@Override public <T> T[] getSpans(int start, int end, Class<T> type) {
-	    return (T[]) new Object[0];
-	}
-	@Override public int nextSpanTransition(int start, int end, Class type) {
-	    return 0;
-	}
-	@Override public char charAt(int index) {
-	    return data.charAt(index);
-	}
-	@Override public int length() {
-	    return data.length();
-	}
-	@Override public CharSequence subSequence(int start, int end) {
-	    return data.subSequence(start, end);
-	}
-	@Override public String toString() {
-	    return data.toString();
-	}
+        @Override public int getSpanStart(Object tag) {
+            return 0;
+        }
+        @Override public int getSpanEnd(Object tag) {
+            return 0;
+        }
+        @Override public int getSpanFlags(Object tag) {
+            return 0;
+        }
+        @Override public <T> T[] getSpans(int start, int end, Class<T> type) {
+            return (T[]) new Object[0];
+        }
+        @Override public int nextSpanTransition(int start, int end, Class type) {
+            return 0;
+        }
+        @Override public char charAt(int index) {
+            return data.charAt(index);
+        }
+        @Override public int length() {
+            return data.length();
+        }
+        @Override public CharSequence subSequence(int start, int end) {
+            return data.subSequence(start, end);
+        }
+        @Override public String toString() {
+            return data.toString();
+        }
     }
 }
