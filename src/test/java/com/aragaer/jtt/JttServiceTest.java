0ef6dd0f74b6c72289c9be6ddb027e052261bf21
aragaer <aragaer@gmail.com>
2017-06-06 13:05:24 +0300

    Create test for JttService

---
+++ src/test/java/com/aragaer/jtt/JttServiceTest.java
@@ -0,0 +1,62 @@
+// -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; -*-
+// vim: et ts=4 sts=4 sw=4 syntax=java
+package com.aragaer.jtt;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.*;
+
+import android.app.Service;
+import android.content.*;
+import android.preference.PreferenceManager;
+import android.os.Handler;
+import android.util.Log;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+
+import com.aragaer.jtt.android.AndroidTicker;
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({AndroidTicker.class, Log.class, Service.class, PreferenceManager.class})
+public class JttServiceTest {
+
+    private static Context mockContext = mock(Context.class);
+    private static SharedPreferences mockPref = mock(SharedPreferences.class);
+    private static Handler mockHandler = mock(Handler.class);
+
+    private TestJttService service;
+
+    @Before public void setUp() throws Exception {
+        mockStatic(Log.class);
+        mockStatic(PreferenceManager.class);
+        when(PreferenceManager.getDefaultSharedPreferences(any(Context.class))).thenReturn(mockPref);
+        when(mockPref.getString(Settings.PREF_LOCATION, "0.0:0.0")).thenReturn("0.0:0.0");
+        suppress(method(Handler.class, "sendEmptyMessage"));
+        suppress(method(Service.class, "onCreate"));
+
+        service = spy(new TestJttService());
+    }
+
+    @Test public void testStart() {
+        int result = service.onStartCommand(null, 0, 0);
+        assertEquals("onStartCommand returns START_STICKY",
+                     Service.START_STICKY, result);
+        assertEquals("Two recievers registered in onStartCommand",
+                     2, service.receiversRegistered);
+    }
+
+    private class TestJttService extends JttService {
+        int receiversRegistered = 0;
+
+        @Override public Intent registerReceiver(BroadcastReceiver receiver,
+                                                 IntentFilter intentFilter) {
+            receiversRegistered++;
+            return null;
+        }
+    }
+}
