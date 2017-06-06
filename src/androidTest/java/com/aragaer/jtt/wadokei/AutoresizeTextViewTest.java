2596dd5bed0e73435094d129d7c0e7fffd55288f
aragaer <aragaer@gmail.com>
2017-06-06 23:59:58 +0300

    Rewrite AutoresizeTextView test
    Old AutoresizeTextViewTest was using deprecated instrumentation test
    API. Rewritten using android test support library.

--- src/androidTest/java/com/aragaer/jtt/wadokei/AutoresizeTextViewTest.java
+++ src/androidTest/java/com/aragaer/jtt/wadokei/AutoresizeTextViewTest.java
@@ -2,15 +2,32 @@
 // vim: et ts=4 sts=4 sw=4 syntax=java
 package com.aragaer.jtt.wadokei;
 
-import android.test.AndroidTestCase;
+import android.content.Context;
+import android.support.test.runner.AndroidJUnit4;
 import android.widget.TextView;
 
+import org.junit.*;
+import org.junit.runner.RunWith;
 
-public class AutoresizeTextViewTest extends AndroidTestCase {
+import static org.junit.Assert.*;
 
-    public void testChangeTextSize() {
-        float scale = getContext().getResources().getDisplayMetrics().density;
-        TextView view = new AutoresizeTextView(getContext());
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+
+
+@RunWith(AndroidJUnit4.class)
+public class AutoresizeTextViewTest {
+
+    private Context context;
+    private TextView view;
+    private float scale;
+
+    @Before public void setUp() {
+        context = getTargetContext();
+        view = new AutoresizeTextView(context);
+        scale = context.getResources().getDisplayMetrics().density;
+    }
+
+    @Test public void testChangeTextSize() {
         view.setTextSize(42);
         view.setText("test");
         view.measure(100, 0);
@@ -18,12 +35,10 @@ public class AutoresizeTextViewTest extends AndroidTestCase {
         assertTrue(view.getTextSize() <= 42*scale);
     }
 
-    public void testKeepSizeIfFits() {
-        float scale = getContext().getResources().getDisplayMetrics().density;
-        TextView view = new AutoresizeTextView(getContext());
+    @Test public void testKeepSizeIfFits() {
         view.setTextSize(12);
         view.setText("test");
         view.measure(100, 0);
-        assertEquals(view.getTextSize(), 12*scale);
+        assertEquals(view.getTextSize(), 12*scale, 0.1);
     }
 }
