c61e1d5800e7fc83bb5389862a4d530a50d06cfd
aragaer <aragaer@gmail.com>
2019-07-14 19:27:53 +0300

    Fix formatting in MainActivity

--- src/main/java/com/aragaer/jtt/JTTMainActivity.java
+++ src/main/java/com/aragaer/jtt/JTTMainActivity.java
@@ -13,44 +13,43 @@ import android.util.Log;
 public class JTTMainActivity extends Activity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override public void onCreate(Bundle savedInstanceState) {
-	setTheme(Settings.getAppTheme(this));
-	super.onCreate(savedInstanceState);
-	startService(new Intent(this, JttService.class));
-	final SharedPreferences pref = PreferenceManager .getDefaultSharedPreferences(this);
-	pref.registerOnSharedPreferenceChangeListener(this);
-	if (savedInstanceState == null) { // Otherwise we think that fragments are saved/restored
-	    getFragmentManager().popBackStackImmediate("settings", FragmentManager.POP_BACK_STACK_INCLUSIVE);
-	    getFragmentManager().beginTransaction()
-		.replace(android.R.id.content, new MainFragment()).commit();
-	    if (!pref.contains(Settings.PREF_LOCATION)) // location is not set
-		openSettings();
-	}
+        setTheme(Settings.getAppTheme(this));
+        super.onCreate(savedInstanceState);
+        startService(new Intent(this, JttService.class));
+        final SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        pref.registerOnSharedPreferenceChangeListener(this);
+        if (savedInstanceState == null) { // Otherwise we think that fragments are saved/restored
+            getFragmentManager().beginTransaction()
+                    .replace(android.R.id.content, new MainFragment()).commit();
+            if (!pref.contains(Settings.PREF_LOCATION)) // location is not set
+                openSettings();
+        }
     }
 
     public void onSharedPreferenceChanged(SharedPreferences pref, String key) {
-	if (key.equals(Settings.PREF_THEME) || key.equals(Settings.PREF_LOCALE))
-	    recreate();
+        if (key.equals(Settings.PREF_THEME) || key.equals(Settings.PREF_LOCALE))
+            recreate();
     }
 
     @Override public void onSaveInstanceState(Bundle outState) {
-	super.onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
     }
 
     private void openSettings() {
-	getFragmentManager().popBackStackImmediate("settings", FragmentManager.POP_BACK_STACK_INCLUSIVE);
-	getFragmentManager().beginTransaction()
-	    .replace(android.R.id.content, new SettingsFragment())
-	    .addToBackStack("settings")
-	    .commit();
+        getFragmentManager().popBackStackImmediate("settings", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .addToBackStack("settings")
+                .commit();
     }
 
     @Override public boolean onCreateOptionsMenu(Menu menu) {
-	menu.add(R.string.settings);
-	return super.onCreateOptionsMenu(menu);
+        menu.add(R.string.settings);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override public boolean onOptionsItemSelected(MenuItem item) {
-	openSettings();
-	return true;
+        openSettings();
+        return true;
     }
 }
