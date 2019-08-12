e68c3ad2211348db0467ad5a18c98ab333654447
aragaer <aragaer@gmail.com>
2019-08-12 21:54:54 +0300

    Fix automatic start at boot time
    Starting from Android O BootReceiver should use startForegroundService
    instead of startService.

--- src/main/java/com/aragaer/jtt/android/BootReceiver.java
+++ src/main/java/com/aragaer/jtt/android/BootReceiver.java
@@ -5,12 +5,18 @@ package com.aragaer.jtt.android;
 import com.aragaer.jtt.JttService;
 
 import android.content.*;
+import android.os.Build;
 
 public class BootReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
-        if (action == null || !action.equals(Intent.ACTION_BOOT_COMPLETED))
+        if (action == null)
             return;
-        context.startService(new Intent(context, JttService.class));
+        if (action.equals(Intent.ACTION_BOOT_COMPLETED)) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                context.startForegroundService(new Intent(context, JttService.class));
+            else
+                context.startService(new Intent(context, JttService.class));
+        }
     }
 }
